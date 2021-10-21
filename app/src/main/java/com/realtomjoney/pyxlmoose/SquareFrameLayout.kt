1411cb2c843f6c7d2066cec88c9ca10476c98421
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 16:52:57 +1300

    Added code to SquareFrameLayout.kt, the fragment will be hosted in that container for the app. The reason I made this class is to represent an extensible FrameLayout whose width and height parameters are the same.

--- app/src/main/java/com/realtomjoney/pyxlmoose/SquareFrameLayout.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/SquareFrameLayout.kt
@@ -1,4 +1,17 @@
 package com.realtomjoney.pyxlmoose
 
-class SquareFrameLayout {
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class SquareFrameLayout : FrameLayout {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attributes: AttributeSet) : super(context, attributes)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val width = measuredWidth
+        setMeasuredDimension(width, width)
+    }
 }
