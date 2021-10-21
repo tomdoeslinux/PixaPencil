4c42ad7c016aa1c0a65c7c369fb5c2fdd0d5f7ee
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 15:03:58 +1300

    Setup bindings in MainActivity.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/MainActivity.kt
@@ -2,10 +2,19 @@ package com.realtomjoney.pyxlmoose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.realtomjoney.pyxlmoose.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setBindings()
+    }
+
+    private fun setBindings() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
