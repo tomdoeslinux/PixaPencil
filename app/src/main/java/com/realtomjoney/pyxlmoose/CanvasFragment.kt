da88eab2885ff69ad3c001b832f520d74d07ba8f
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 16:49:57 +1300

    Set up binding in CanvasFragment.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/CanvasFragment.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/CanvasFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.realtomjoney.pyxlmoose.databinding.FragmentCanvasBinding
 
 class CanvasFragment : Fragment() {
+    private var _binding: FragmentCanvasBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_canvas, container, false)
+        _binding = FragmentCanvasBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
