070ef820968ff28138b2a6fe029d5d7ae34c94a0
xTheCoderx <justaguyoney@gmail.com>
2021-10-22 10:06:40 +1300

    Fixed issue of slits being visible between pixels by removing the onMeasure() function in the Pixel class.

--- app/src/main/java/com/realtomjoney/pyxlmoose/Pixel.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/Pixel.kt
@@ -7,11 +7,4 @@ import android.view.View
 class Pixel : View {
     constructor(context: Context) : super(context)
     constructor(context: Context, attributes: AttributeSet) : super(context, attributes)
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-
-        val width = measuredWidth
-        setMeasuredDimension(width, width)
-    }
 }
