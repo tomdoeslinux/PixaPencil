c98dd7396b01322880f55104580818a931e9d289
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 17:24:50 +1300

    CanvasRecyclerAdapter.kt now derives from RecyclerView.Adapter<RecyclerViewHolder> (RecyclerViewHolder) is our custom RecyclerView.ViewHolder implementation.

--- app/src/main/res/layout/CanvasRecyclerAdapter.kt
+++ app/src/main/res/layout/CanvasRecyclerAdapter.kt
@@ -1,8 +1,30 @@
 package layout
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.realtomjoney.pyxlmoose.CanvasFragmentListener
 import com.realtomjoney.pyxlmoose.Pixel
+import com.realtomjoney.pyxlmoose.R
+import com.realtomjoney.pyxlmoose.SquareFrameLayout
+
+class RecyclerViewHolder(inflater: LayoutInflater, parent: ViewGroup)
+    : RecyclerView.ViewHolder(inflater.inflate(R.layout.pixel_layout, parent, false)) {
+        val tileParent: SquareFrameLayout = itemView.findViewById(R.id.pixelParent)
+}
 
 class CanvasRecyclerAdapter(private val pixels: List<Pixel>,
-                            private val caller: CanvasFragmentListener) {
+                            private val caller: CanvasFragmentListener) :
+    RecyclerView.Adapter<RecyclerViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
