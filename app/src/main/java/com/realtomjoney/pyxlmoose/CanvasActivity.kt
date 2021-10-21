3e3f7fea9549daf27c73a2b54775831d7e7374a6
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 15:09:41 +1300

    Added CanvasActivity.kt which will contain a fragment.

---
+++ app/src/main/java/com/realtomjoney/pyxlmoose/CanvasActivity.kt
@@ -0,0 +1,11 @@
+package com.realtomjoney.pyxlmoose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CanvasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_canvas)
+    }
+}
