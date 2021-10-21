fe8e2ceddd6401485238ff6c45997a745bb416e0
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 17:43:25 +1300

    Added code for onDestroyView in CanvasFragment.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/CanvasFragment.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/CanvasFragment.kt
@@ -25,4 +25,9 @@ class CanvasFragment : Fragment() {
             return CanvasFragment()
         }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
