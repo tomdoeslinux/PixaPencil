2d21657e0c8cf3704930e25ddbb92b6e20d2fabd
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 15:55:06 +1300

    Set bindings for CanvasActivity.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/CanvasActivity.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/CanvasActivity.kt
@@ -2,10 +2,18 @@ package com.realtomjoney.pyxlmoose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.realtomjoney.pyxlmoose.databinding.ActivityCanvasBinding
 
 class CanvasActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCanvasBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_canvas)
+        setBindings()
+    }
+
+    private fun setBindings() {
+        binding = ActivityCanvasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
