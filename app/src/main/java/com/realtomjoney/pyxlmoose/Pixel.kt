c47cadcdaf2472e92ab0b13d6d1d181c2ddfff5a
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 15:49:24 +1300

    Added Pixel.kt class which extends View.

--- app/src/main/java/com/realtomjoney/pyxlmoose/Pixel.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/Pixel.kt
@@ -1,4 +1,17 @@
 package com.realtomjoney.pyxlmoose
 
-class Pixel {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class Pixel : View {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attributes: AttributeSet) : super(context, attributes)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val width = measuredWidth
+        setMeasuredDimension(width, width)
+    }
 }
