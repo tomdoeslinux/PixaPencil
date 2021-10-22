c17aec41a693ae8b0b4ac72c0008338b1c8c80cc
xTheCoderx <justaguyoney@gmail.com>
2021-10-22 22:01:09 +1300

    Fixed warnings in CanvasFragment.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/CanvasFragment.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/CanvasFragment.kt
@@ -1,14 +1,11 @@
 package com.realtomjoney.pyxlmoose
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.drawToBitmap
 import androidx.recyclerview.widget.GridLayoutManager
 import com.realtomjoney.pyxlmoose.databinding.FragmentCanvasBinding
 
