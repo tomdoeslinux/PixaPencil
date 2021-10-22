1d1ad97351a468bc79ef529e1bc85db48f78c662
xTheCoderx <justaguyoney@gmail.com>
2021-10-22 17:19:56 +1300

    Refactored code to use a RecyclerView as the colour picker instead of individual Views.

--- app/src/main/java/com/realtomjoney/pyxlmoose/CanvasActivity.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/CanvasActivity.kt
@@ -1,39 +1,41 @@
 package com.realtomjoney.pyxlmoose
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.realtomjoney.pyxlmoose.databinding.ActivityCanvasBinding
+
+var colour: Int = Color.BLACK
 
 class CanvasActivity : AppCompatActivity(), CanvasFragmentListener {
     private lateinit var binding: ActivityCanvasBinding
-
-    private var colour: Int = Color.BLACK
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setBindings()
         setUpFragment()
-        setColourClickListeners()
+        setUpRecyclerView()
     }
 
-    private fun setColourClickListeners() {
-        binding.colorRed.setOnClickListener {
-            colour = (it.background as ColorDrawable).color
-        }
-        binding.colorGreen.setOnClickListener {
-            colour = (it.background as ColorDrawable).color
-        }
-        binding.colorBlue.setOnClickListener {
-            colour = (it.background as ColorDrawable).color
-        }
-        binding.colorPurple.setOnClickListener {
-            colour = (it.background as ColorDrawable).color
-        }
-        binding.colorPink.setOnClickListener {
-            colour = (it.background as ColorDrawable).color
-        }
+    private fun setUpRecyclerView() {
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        binding.colourPickerRecyclerView.layoutManager = layoutManager
+
+        binding.colourPickerRecyclerView.adapter = MyAdapter(listOf(
+            Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.MAGENTA, Color.CYAN, Color.BLACK, Color.argb(100, 255, 69, 0)), binding.rootLayout)
     }
 
     private fun setUpFragment() {
@@ -59,3 +61,30 @@ class CanvasActivity : AppCompatActivity(), CanvasFragmentListener {
         pixel.setBackgroundColor(colour)
     }
 }
+
+class MyAdapter(private val list: List<Int>,
+                private val rootLayout: View) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val textView = LayoutInflater.from(parent.context)
+            .inflate(
+                R.layout.colour_picker_layout,
+                parent,
+                false
+            ) as View
+
+        return MyViewHolder(textView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.view.setBackgroundColor(list[position])
+
+        holder.view.setOnClickListener {
+            colour = list[position]
+        }
+    }
+
+    override fun getItemCount() = list.size
+}
+
