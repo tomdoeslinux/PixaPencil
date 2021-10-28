273fd86610c15d4f16542faf51ccb77ea4c0ac3e
xTheCoderx <justaguyoney@gmail.com>
2021-10-28 15:32:33 +1300

    Made some major UI changes in recent_creations_layout.xml - the content is now within a CardView which is wrapped inside a FrameLayout.

--- app/src/main/java/com/realtomjoney/pyxlmoose/RecentCreationsAdapter.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/RecentCreationsAdapter.kt
@@ -1,27 +1,31 @@
 package com.realtomjoney.pyxlmoose
 
-import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class RecentCreationsAdapter(private val data: List<SavedPixelArt>, private val listener: RecentCreationsListener) : RecyclerView.Adapter<RecentCreationsAdapter.RecentCreationsViewHolder>() {
-    class RecentCreationsViewHolder(val constraintLayout: ConstraintLayout) : RecyclerView.ViewHolder(constraintLayout)
+    class RecentCreationsViewHolder(val frame: FrameLayout) : RecyclerView.ViewHolder(frame)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        RecentCreationsViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recent_creations_layout, parent, false) as ConstraintLayout)
+        RecentCreationsViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recent_creations_layout, parent, false) as FrameLayout)
 
     override fun onBindViewHolder(holder: RecentCreationsViewHolder, position: Int) = data.forEach { _ ->
-        holder.constraintLayout.findViewById<ImageView>(R.id.mImageView).setImageBitmap(data[position].bitmap)
-        holder.constraintLayout.findViewById<TextView>(R.id.mtext).text = data[position].title
-        holder.constraintLayout.findViewById<TextView>(R.id.mdate).text = data[position].dateCreated
+        val rootLayout = holder.frame.findViewById<FrameLayout>(R.id.rootFrameLayout)
+        with (rootLayout.findViewById<CardView>(R.id.mCard))  {
+            findViewById<ImageView>(R.id.mImageView).setImageBitmap(data[position].bitmap)
+            findViewById<TextView>(R.id.mtext).text = data[position].title
+            findViewById<ConstraintLayout>(R.id.recentCreationsLayout).findViewById<TextView>(R.id.mdate).text = data[position].dateCreated
 
-        holder.constraintLayout.setOnClickListener {
-            listener.onCreationTapped(BitmapDatabase.toList()[position])
+            setOnClickListener {
+                listener.onCreationTapped(BitmapDatabase.toList()[position])
+            }
         }
     }
 
