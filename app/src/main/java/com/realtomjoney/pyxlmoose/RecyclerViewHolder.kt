a4b7bfcdf0ead0fa4634accc40c4a744f0bf1dab
xTheCoderx <justaguyoney@gmail.com>
2021-10-28 18:07:04 +1300

    Fixed warnings in RecyclerViewHolder.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/RecyclerViewHolder.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/RecyclerViewHolder.kt
@@ -1,7 +1,5 @@
 package com.realtomjoney.pyxlmoose
 
-import android.graphics.Color
-import android.opengl.Visibility
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
