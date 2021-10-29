5e01e8017fade20d83528e6fcb969b1b9d12a284
xTheCoderx <justaguyoney@gmail.com>
2021-10-30 01:03:41 +1300

    Cleaned up code in RecentCreationsAdapter.kt. Moved RecentCreationsViewHolder.kt to a separate file.

--- app/src/main/java/com/realtomjoney/pyxlmoose/RecentCreationsAdapter.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/RecentCreationsAdapter.kt
@@ -12,50 +12,51 @@ import android.text.TextUtils
 import android.view.View
 import android.widget.ImageButton
 import com.google.android.material.snackbar.Snackbar
+import com.realtomjoney.pyxlmoose.databinding.ActivityMainBinding
+import com.realtomjoney.pyxlmoose.databinding.RecentCreationsLayoutBinding
 
-class RecentCreationsAdapter(private var data: List<PixelArt>, private val listener: RecentCreationsListener) : RecyclerView.Adapter<RecentCreationsAdapter.RecentCreationsViewHolder>() {
-    class RecentCreationsViewHolder(val frame: FrameLayout) : RecyclerView.ViewHolder(frame)
+class RecentCreationsAdapter(private var data: List<PixelArt>, private val listener: RecentCreationsListener) : RecyclerView.Adapter<RecentCreationsViewHolder>() {
+    private lateinit var binding: RecentCreationsLayoutBinding
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        RecentCreationsViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recent_creations_layout, parent, false) as FrameLayout)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecentCreationsViewHolder {
+        binding = RecentCreationsLayoutBinding.inflate(LayoutInflater.from(parent.context))
+        return RecentCreationsViewHolder(binding.rootFrameLayout)
+    }
 
     override fun onBindViewHolder(holder: RecentCreationsViewHolder, position: Int) = data.forEach { _ ->
-        val rootLayout = holder.frame.findViewById<FrameLayout>(R.id.rootFrameLayout)
+        with (binding.mCard)  {
+            val item = data[position]
 
-
-        with (rootLayout.findViewById<CardView>(R.id.mCard))  {
-            findViewById<ImageView>(R.id.mImageView).setImageBitmap(data[position].bitmap)
-            findViewById<TextView>(R.id.mdate).text = data[position].dateCreated
-            val title: TextView = findViewById(R.id.mtext)
+            binding.mImageView.setImageBitmap(item.bitmap)
+            binding.mdate.text = item.dateCreated
 
             if (data[position].title.length > 6) {
-                title.ellipsize = TextUtils.TruncateAt.MARQUEE
-                title.isSelected = true
-                title.isSingleLine = true
-                title.text = (data[position].title + "                    ").repeat(200)
+                binding.mtext.ellipsize = TextUtils.TruncateAt.MARQUEE
+                binding.mtext.isSelected = true
+                binding.mtext.isSingleLine = true
+                binding.mtext.text = (item.title + " ".repeat(10)).repeat(200)
             } else {
-                title.text = data[position].title
+                binding.mtext.text = item.title
             }
 
             this.setOnClickListener {
-                listener.onCreationTapped(data[position])
+                listener.onCreationTapped(item)
             }
 
             this.setOnLongClickListener {
-                listener.onCreationLongTapped(data[position])
+                listener.onCreationLongTapped(item)
                 true
             }
 
-            changeStarredIndicator((findViewById(R.id.mFavouriteButton)), data[position])
+            changeStarredIndicator(binding.mFavouriteButton, item)
 
-            this.findViewById<ImageButton>(R.id.mFavouriteButton).setOnClickListener {
-                if (!data[position].isFavourited) favouriteRecentCreation(this, data[position])
-                else unFavouriteRecentCreation(this, data[position])
+            binding.mFavouriteButton.setOnClickListener {
+                if (item.isFavourited) unFavouriteRecentCreation(this, item)
+                else favouriteRecentCreation(this, item)
 
-                changeStarredIndicator((it as ImageButton), data[position])
+                changeStarredIndicator((it as ImageButton), item)
             }
         }
-
     }
 
     private fun changeStarredIndicator(imageButton: ImageButton, pixelArt: PixelArt) {
@@ -63,24 +64,12 @@ class RecentCreationsAdapter(private var data: List<PixelArt>, private val liste
         else imageButton.setImageResource(R.drawable.ic_baseline_star_border_24)
     }
     private fun favouriteRecentCreation(contextView: View, pixelArt: PixelArt) {
-        Snackbar.make(contextView,
-            "Saved ${pixelArt.title} to favourites.",
-            Snackbar.LENGTH_LONG)
-            .setTextColor(Color.BLACK)
-            .setBackgroundTint(Color.parseColor("#eaddff"))
-            .show()
-
+        contextView.showSnackbar("Saved ${pixelArt.title} to favourites.", SnackbarDuration.DEFAULT)
         pixelArt.isFavourited = true
     }
 
     private fun unFavouriteRecentCreation(contextView: View, pixelArt: PixelArt) {
-        Snackbar.make(contextView,
-            "You have removed ${pixelArt.title} from your favourites.",
-            Snackbar.LENGTH_LONG)
-            .setTextColor(Color.BLACK)
-            .setBackgroundTint(Color.parseColor("#eaddff"))
-            .show()
-
+        contextView.showSnackbar("You have removed ${pixelArt.title} from your favourites.", SnackbarDuration.DEFAULT)
         pixelArt.isFavourited = false
     }
 
---
+++ app/src/main/java/com/realtomjoney/pyxlmoose/RecentCreationsViewHolder.kt
@@ -0,0 +1,6 @@
+package com.realtomjoney.pyxlmoose
+
+import android.widget.FrameLayout
+import androidx.recyclerview.widget.RecyclerView
+
+class RecentCreationsViewHolder(val frame: FrameLayout) : RecyclerView.ViewHolder(frame)
