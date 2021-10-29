9f68b9340b8560d8d1b94ea2ca5579bb1258cf65
xTheCoderx <justaguyoney@gmail.com>
2021-10-29 16:17:41 +1300

    Removed unused import directives in MainActivity.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/MainActivity.kt
@@ -4,19 +4,12 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.EditText
-import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.realtomjoney.pyxlmoose.databinding.ActivityMainBinding
 import java.util.*
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
-
-
-
-
 
 class MainActivity : AppCompatActivity(), RecentCreationsListener {
 
