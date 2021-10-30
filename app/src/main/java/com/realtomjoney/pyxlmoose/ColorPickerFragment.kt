20390b2061daae35258be61c9d88023e36165674
xTheCoderx <justaguyoney@gmail.com>
2021-10-30 14:40:12 +1300

    Setup ViewBinding for ColorPickerFragment.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/ColorPickerFragment.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/ColorPickerFragment.kt
@@ -5,17 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.realtomjoney.pyxlmoose.databinding.FragmentColorPickerBinding
 
 class ColorPickerFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_color_picker, container, false)
-    }
+    private var _binding: FragmentColorPickerBinding? = null
+
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = ColorPickerFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentColorPickerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
