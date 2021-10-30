238128b890be5920c375e3514efc7197b0171563
xTheCoderx <justaguyoney@gmail.com>
2021-10-30 18:44:54 +1300

    Cleaned up code in MainActivity.kt (1).

--- app/src/main/java/com/realtomjoney/pyxlmoose/activity/main/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activity/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.realtomjoney.pyxlmoose.activity
+package com.realtomjoney.pyxlmoose.activity.main
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -12,7 +12,7 @@ import com.realtomjoney.pyxlmoose.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), RecentCreationsListener {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     private var hasNavigatedBack = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,46 +26,7 @@ class MainActivity : AppCompatActivity(), RecentCreationsListener {
         title = "Home"
     }
 
-    private fun setOnClickListeners() {
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.page_home -> {
-                    binding.recentCreationsRecyclerView.adapter = RecentCreationsAdapter(
-                        PixelArtDatabase.toList(), this)
-                    title = "Home"
-                }
-                R.id.page_starred -> {
-                    binding.recentCreationsRecyclerView.adapter = RecentCreationsAdapter(
-                        PixelArtDatabase.toList().filter { it.isFavourited }, this)
-                    title = "Favorites"
-                }
-            }
-            true
-        }
-
-        binding.recentCreationsRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                if (dy == 1 || dy == 0) return
-
-                if (binding.floatingActionButton.isShown && dy > 2 || dy < 2) binding.floatingActionButton.hide() else binding.floatingActionButton.show()
-            }
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) binding.floatingActionButton.show()
-            }
-        }) // Great solution by VelocityPulse (with a small twist from myself)
-
-        binding.floatingActionButton.setOnClickListener {
-            val dialogueEditText = EditText(this)
-            dialogueEditText.hint = "Span count"
-
-            showDialog(
-                "Span count",
-                "Please input an appropriate span count value:",
-                "Done",
-                { _, _ -> startActivity(Intent(this, CanvasActivity::class.java).putExtra("SPAN_COUNT", Integer.parseInt(dialogueEditText.text.toString())))
-                }, "Back", { _, _ -> }, dialogueEditText)
-        }
-    }
+    private fun setOnClickListeners() = extendedSetOnClickListeners()
 
     override fun onResume() {
         binding.floatingActionButton.show()
