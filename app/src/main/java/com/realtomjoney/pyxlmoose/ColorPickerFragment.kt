dc118107ff176315c526ebcf0374db6a059390be
xTheCoderx <justaguyoney@gmail.com>
2021-10-31 12:15:00 +1300

    Removed unused import directive in ColorPickerFragment.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/ColorPickerFragment.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/ColorPickerFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.realtomjoney.pyxlmoose.databinding.FragmentColorPickerBinding
 
 class ColorPickerFragment : Fragment() {
