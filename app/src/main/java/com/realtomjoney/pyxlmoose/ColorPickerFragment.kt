f828bba17fae83eaba1ac184fc61b815ce851ec3
xTheCoderx <justaguyoney@gmail.com>
2021-10-30 15:06:01 +1300

    Added basic logic/code to ColorPickerFragment.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/ColorPickerFragment.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/ColorPickerFragment.kt
@@ -1,5 +1,6 @@
 package com.realtomjoney.pyxlmoose
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,12 +14,37 @@ class ColorPickerFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+    private var valueR = 0
+    private var valueG = 0
+    private var valueB = 0
+
     companion object {
         fun newInstance() = ColorPickerFragment()
     }
 
+    private fun updateColorSelectedPreview() =  binding.colorPickerPreview.setBackgroundColor(Color.argb(255, valueR, valueG, valueB))
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentColorPickerBinding.inflate(inflater, container, false)
+
+        binding.colorPickerPreview.setBackgroundColor(Color.argb(255, valueR, valueG, valueB))
+
+        binding.redProgressBar.addOnChangeListener { _, value, _ ->
+            valueR = value.toInt()
+            updateColorSelectedPreview()
+        }
+
+        binding.greenProgressBar.addOnChangeListener { _, value, _ ->
+            valueG = value.toInt()
+            updateColorSelectedPreview()
+        }
+
+        binding.blueProgressBar.addOnChangeListener { _, value, _ ->
+            valueB = value.toInt()
+            updateColorSelectedPreview()
+        }
+
         return binding.root
     }
 
