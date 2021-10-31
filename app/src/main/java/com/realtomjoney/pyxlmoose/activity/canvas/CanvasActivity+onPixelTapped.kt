6e17671d4a17c3304d292adf77d72f88b4b36d05
xTheCoderx <justaguyoney@gmail.com>
2021-10-31 14:46:28 +1300

    Fixed mirror tool not working bug by adding line 9 to CanvasActivity+onPixelTapped.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activity/canvas/CanvasActivity+onPixelTapped.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activity/canvas/CanvasActivity+onPixelTapped.kt
@@ -6,6 +6,7 @@ import android.view.View
 
 fun CanvasActivity.extendedOnPixelTapped(pixel: View) {
     if (isMirrorMode && !colorPickerMode) {
+        pixel.setBackgroundColor(getSelectedColour())
         data[((data.indexOf(pixel)) - ((data.indexOf(pixel)).mod(spanCount))) + (spanCount -  ((data.indexOf(pixel)).mod(spanCount))) - 1].setBackgroundColor(
             getSelectedColour()
         ) // Credits to PapaBread for this masterpiece of a solution
