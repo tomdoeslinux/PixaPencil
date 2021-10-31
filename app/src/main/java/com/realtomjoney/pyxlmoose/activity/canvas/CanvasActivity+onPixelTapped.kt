3bd2ec9156132d3eea837aa8edd4f023f4ec7104
xTheCoderx <justaguyoney@gmail.com>
2021-10-31 14:54:07 +1300

    Reformatted code in CanvasActivity+onPixelTapped.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activity/canvas/CanvasActivity+onPixelTapped.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activity/canvas/CanvasActivity+onPixelTapped.kt
@@ -1,18 +1,14 @@
 package com.realtomjoney.pyxlmoose.activity.canvas
 
 import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
 import android.view.View
 
 fun CanvasActivity.extendedOnPixelTapped(pixel: View) {
     if (isMirrorMode && !colorPickerMode) {
         pixel.setBackgroundColor(getSelectedColour())
-        data[((data.indexOf(pixel)) - ((data.indexOf(pixel)).mod(spanCount))) + (spanCount -  ((data.indexOf(pixel)).mod(spanCount))) - 1].setBackgroundColor(
-            getSelectedColour()
-        ) // Credits to PapaBread for this masterpiece of a solution
+        data[((data.indexOf(pixel)) - ((data.indexOf(pixel)).mod(spanCount))) + (spanCount -  ((data.indexOf(pixel)).mod(spanCount))) - 1].setBackgroundColor(getSelectedColour()) // Credits to PapaBread for this masterpiece of a solution
     }
     else if (colorPickerMode) {
-        val background: Drawable? = pixel.background
-        background?.let { setPixelColour((it as ColorDrawable).color) }
+        (pixel.background)?.let { setPixelColour((it as ColorDrawable).color) }
     } else { pixel.setBackgroundColor(getSelectedColour()) }
 }
