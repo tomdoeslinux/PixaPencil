347e934478f1baad21baf8a490b3084296f37707
xTheCoderx <justaguyoney@gmail.com>
2021-10-31 16:12:13 +1300

    Fixed bug in which mirror tool was not working.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activity/canvas/CanvasActivity+setUpFragment.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activity/canvas/CanvasActivity+setUpFragment.kt
@@ -4,6 +4,7 @@ import com.realtomjoney.pyxlmoose.CanvasFragment
 import com.realtomjoney.pyxlmoose.R
 
 fun CanvasActivity.extendedSetUpFragment() {
+    instance2 = CanvasFragment.newInstance(spanCount, true, null)
     supportFragmentManager
         .beginTransaction()
         .add(R.id.fragmentHost, instance2).commit()
