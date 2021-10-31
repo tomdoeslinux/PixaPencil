9e606d6838017625d97e6e19d09bc9d1d737fa19
xTheCoderx <justaguyoney@gmail.com>
2021-10-31 17:25:03 +1300

    Fixed bug in which certain colours were having the effect as having being selected when they were not selected.

--- app/src/main/java/com/realtomjoney/pyxlmoose/ColourPickerAdapter.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/ColourPickerAdapter.kt
@@ -1,6 +1,9 @@
 package com.realtomjoney.pyxlmoose
 
 import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,10 +15,15 @@ class ColourPickerAdapter(private val list: List<Int>, private val caller: Colou
     }
 
     override fun onBindViewHolder(holder: ColourPickerViewHolder, position: Int) {
+        holder.colourView.setBackgroundResource(R.drawable.round)
         holder.colourView.backgroundTintList = ColorStateList.valueOf(list[position])
 
         holder.colourView.setOnClickListener {
             caller.onColourTapped(list[position], it)
+        }
+
+        if (position == 0) {
+            caller.onColourTapped(list[position], holder.colourView)
         }
     }
 
