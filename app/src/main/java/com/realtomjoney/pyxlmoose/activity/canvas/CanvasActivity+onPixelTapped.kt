12a0cc4270570545fca010167e0202378264ef7b
xTheCoderx <justaguyoney@gmail.com>
2021-10-31 14:55:00 +1300

    Reformatted code in CanvasActivity+onPixelTapped.kt (2).

--- app/src/main/java/com/realtomjoney/pyxlmoose/activity/canvas/CanvasActivity+onPixelTapped.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activity/canvas/CanvasActivity+onPixelTapped.kt
@@ -7,8 +7,7 @@ fun CanvasActivity.extendedOnPixelTapped(pixel: View) {
     if (isMirrorMode && !colorPickerMode) {
         pixel.setBackgroundColor(getSelectedColour())
         data[((data.indexOf(pixel)) - ((data.indexOf(pixel)).mod(spanCount))) + (spanCount -  ((data.indexOf(pixel)).mod(spanCount))) - 1].setBackgroundColor(getSelectedColour()) // Credits to PapaBread for this masterpiece of a solution
-    }
-    else if (colorPickerMode) {
+    } else if (colorPickerMode) {
         (pixel.background)?.let { setPixelColour((it as ColorDrawable).color) }
     } else { pixel.setBackgroundColor(getSelectedColour()) }
 }
