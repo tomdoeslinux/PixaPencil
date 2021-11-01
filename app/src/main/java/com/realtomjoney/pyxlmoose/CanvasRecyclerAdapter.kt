ae310c41d398a9b04eb1b99de995882f2eeaaaf7
xTheCoderx <justaguyoney@gmail.com>
2021-11-01 21:11:45 +1300

    Removed unused import directive in CanvasRecyclerAdapter.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/CanvasRecyclerAdapter.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/CanvasRecyclerAdapter.kt
@@ -6,7 +6,6 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.realtomjoney.pyxlmoose.activity.canvas.binding
 
 @SuppressLint("ClickableViewAccessibility")
 
