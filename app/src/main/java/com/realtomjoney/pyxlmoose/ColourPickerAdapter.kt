86a58fcb46c678432c8117068c9b6fd3bef0ebf1
xTheCoderx <justaguyoney@gmail.com>
2021-11-03 02:37:28 +1300

    Removed unused import directives in ColourPickerAdapter.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/ColourPickerAdapter.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/ColourPickerAdapter.kt
@@ -1,9 +1,6 @@
 package com.realtomjoney.pyxlmoose
 
 import android.content.res.ColorStateList
-import android.graphics.Color
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
