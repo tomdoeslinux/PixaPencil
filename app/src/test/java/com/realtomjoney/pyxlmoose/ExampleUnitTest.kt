5678b81da72d8c85206fce744737399680aa3748
xTheCoderx <justaguyoney@gmail.com>
2021-11-02 21:41:11 +1300

    Removed unused import directives in ExampleUnitTest.kt.

--- app/src/test/java/com/realtomjoney/pyxlmoose/ExampleUnitTest.kt
+++ app/src/test/java/com/realtomjoney/pyxlmoose/ExampleUnitTest.kt
@@ -1,8 +1,6 @@
 package com.realtomjoney.pyxlmoose
 
 import android.graphics.Color
-import android.support.v4.media.session.PlaybackStateCompat
-import android.widget.LinearLayout
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.realtomjoney.pyxlmoose.activity.canvas.CanvasActivity
@@ -12,10 +10,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
-import org.junit.Rule
-
-
-
 
 /**
  * Example local unit test, which will execute on the development machine (host).
