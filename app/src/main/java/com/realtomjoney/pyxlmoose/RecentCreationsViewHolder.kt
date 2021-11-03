51c9cf968b6b0ba62a0ece904e1fbfd30e773811
xTheCoderx <justaguyoney@gmail.com>
2021-11-03 15:58:03 +1300

    Fixed warnings in RecentCreationsViewHolder.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/RecentCreationsViewHolder.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/RecentCreationsViewHolder.kt
@@ -3,4 +3,4 @@ package com.realtomjoney.pyxlmoose
 import android.widget.FrameLayout
 import androidx.recyclerview.widget.RecyclerView
 
-class RecentCreationsViewHolder(val frame: FrameLayout) : RecyclerView.ViewHolder(frame)
+class RecentCreationsViewHolder(frame: FrameLayout) : RecyclerView.ViewHolder(frame)
