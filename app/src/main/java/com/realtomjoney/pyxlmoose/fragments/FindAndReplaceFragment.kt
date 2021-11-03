d5feac9d56d396ba137eb173514aff1e58cad2f9
xTheCoderx <justaguyoney@gmail.com>
2021-11-03 17:26:48 +1300

    Created and setup bindings in FindAndReplaceFragment.kt. Replaced root layout in fragment_find_and_replace.xml with a ConstraintLayout instead of the default FrameLayout.

---
+++ app/src/main/java/com/realtomjoney/pyxlmoose/fragments/FindAndReplaceFragment.kt
@@ -0,0 +1,35 @@
+package com.realtomjoney.pyxlmoose.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.realtomjoney.pyxlmoose.R
+import com.realtomjoney.pyxlmoose.databinding.FragmentColorPickerBinding
+import com.realtomjoney.pyxlmoose.databinding.FragmentFindAndReplaceBinding
+
+class FindAndReplaceFragment : Fragment() {
+
+    private var _binding: FragmentFindAndReplaceBinding? = null
+
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = FindAndReplaceFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFindAndReplaceBinding.inflate(inflater, container, false)
+        
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
