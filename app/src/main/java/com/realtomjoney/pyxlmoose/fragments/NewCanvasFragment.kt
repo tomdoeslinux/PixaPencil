c132aaef8d59ce39ab542580ad4e56ead7d49585
xTheCoderx <justaguyoney@gmail.com>
2021-11-26 02:16:51 +1300

    Added NewCanvasFragment.kt. More code will be added in future commits.

---
+++ app/src/main/java/com/realtomjoney/pyxlmoose/fragments/NewCanvasFragment.kt
@@ -0,0 +1,22 @@
+package com.realtomjoney.pyxlmoose.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.realtomjoney.pyxlmoose.R
+
+class NewCanvasFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_new_canvas, container, false)
+    }
+
+    companion object {
+        fun newInstance() = NewCanvasFragment()
+    }
+}
