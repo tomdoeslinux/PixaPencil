da4e8c2f15c99b057d9cc48ed682c58a29b405fb
xTheCoderx <justaguyoney@gmail.com>
2021-11-27 19:20:46 +1300

    Removed unused import directives in MainActivity.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.textfield.TextInputEditText
