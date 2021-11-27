f9b02f2c521cf201b459978274e8f71fb847a2ba
xTheCoderx <justaguyoney@gmail.com>
2021-11-27 19:12:16 +1300

    Removed unused import directives in MainActivity.kt.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.realtomjoney.pyxlmoose.activities.main
 
-import android.app.Activity
 import android.content.Intent
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,11 +9,8 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.textfield.TextInputEditText
-import com.realtomjoney.pyxlmoose.R
 import com.realtomjoney.pyxlmoose.activities.canvas.CanvasActivity
 import com.realtomjoney.pyxlmoose.databinding.ActivityMainBinding
-import com.realtomjoney.pyxlmoose.extensions.SnackbarDuration
-import com.realtomjoney.pyxlmoose.extensions.showSnackbar
 import com.realtomjoney.pyxlmoose.fragments.NewCanvasFragment
 import com.realtomjoney.pyxlmoose.listeners.NewCanvasFragmentListener
 import com.realtomjoney.pyxlmoose.listeners.RecentCreationsListener
