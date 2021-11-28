14f062200a212ce256867f13c93eefabd250c774
xTheCoderx <justaguyoney@gmail.com>
2021-11-28 18:05:13 +1300

    Removed unused import directives in MainActivityTest.kt.

--- app/src/androidTest/java/com/realtomjoney/pyxlmoose/activities/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/realtomjoney/pyxlmoose/activities/main/MainActivityTest.kt
@@ -1,10 +1,7 @@
 package com.realtomjoney.pyxlmoose.activities.main
 
-
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
