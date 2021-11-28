dbe53233f12afc6eba1252114814a2d1ccc91f95
xTheCoderx <justaguyoney@gmail.com>
2021-11-29 01:33:14 +1300

    Refactored tests from MainActivityTest.kt to MainActivityTestsWhenNewProjectIsCreated.kt.

--- app/src/androidTest/java/com/realtomjoney/pyxlmoose/activities/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/realtomjoney/pyxlmoose/activities/main/MainActivityTest.kt
@@ -9,8 +9,6 @@ import com.realtomjoney.pyxlmoose.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 
@@ -53,43 +51,6 @@ class MainActivityTest {
 
     @Test
     fun uitest_fragmentNewCanvasDoneButton_isNotDisplayed() {
-        onView(withId(R.id.fragmentNewCanvas_doneButton)).check(doesNotExist())
-    }
-
-    private fun createNewProject() {
-        onView(withId(R.id.floatingActionButton)).perform(click())
-        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).perform(replaceText("Untitled Project"))
-        onView(withId(R.id.fragmentNewCanvas_spanCountTextInputEditText)).perform(replaceText("5"))
-        onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
-    }
-
-    @Test
-    fun uitest_fragmentNewCanvasProjectTitleTextInputEditText_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
-        createNewProject()
-        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).check(doesNotExist())
-    }
-
-    @Test
-    fun uitest_fragmentNewCanvasProjectTitleTextInputLayout_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
-        createNewProject()
-        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputLayout)).check(doesNotExist())
-    }
-
-    @Test
-    fun uitest_fragmentNewCanvasSpanCountTextInputEditText_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
-        createNewProject()
-        onView(withId(R.id.fragmentNewCanvas_spanCountTextInputEditText)).check(doesNotExist())
-    }
-
-    @Test
-    fun uitest_fragmentNewCanvasSpanCountTextInputLayout_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
-        createNewProject()
-        onView(withId(R.id.fragmentNewCanvas_spanCountTextInputLayout)).check(doesNotExist())
-    }
-
-    @Test
-    fun uitest_fragmentNewCanvasDoneButton_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
-        createNewProject()
         onView(withId(R.id.fragmentNewCanvas_doneButton)).check(doesNotExist())
     }
 
---
+++ app/src/androidTest/java/com/realtomjoney/pyxlmoose/activities/main/MainActivityTestsWhenNewProjectIsCreated.kt
@@ -0,0 +1,59 @@
+package com.realtomjoney.pyxlmoose.activities.main
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.realtomjoney.pyxlmoose.R
+import org.junit.Rule
+import org.junit.Test
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.filters.LargeTest
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.ActivityTestRule
+import org.junit.runner.RunWith
+
+
+@LargeTest
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTestsWhenNewProjectIsCreated {
+    @get:Rule
+    var activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    private fun createNewProject() {
+        onView(withId(R.id.floatingActionButton)).perform(click())
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).perform(ViewActions.replaceText("Untitled Project"))
+        onView(withId(R.id.fragmentNewCanvas_spanCountTextInputEditText)).perform(ViewActions.replaceText("5"))
+        onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
+    }
+
+    @Test
+    fun uitest_fragmentNewCanvasProjectTitleTextInputEditText_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
+        createNewProject()
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).check(ViewAssertions.doesNotExist())
+    }
+
+    @Test
+    fun uitest_fragmentNewCanvasProjectTitleTextInputLayout_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
+        createNewProject()
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputLayout)).check(ViewAssertions.doesNotExist())
+    }
+
+    @Test
+    fun uitest_fragmentNewCanvasSpanCountTextInputEditText_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
+        createNewProject()
+        onView(withId(R.id.fragmentNewCanvas_spanCountTextInputEditText)).check(ViewAssertions.doesNotExist())
+    }
+
+    @Test
+    fun uitest_fragmentNewCanvasSpanCountTextInputLayout_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
+        createNewProject()
+        onView(withId(R.id.fragmentNewCanvas_spanCountTextInputLayout)).check(ViewAssertions.doesNotExist())
+    }
+
+    @Test
+    fun uitest_fragmentNewCanvasDoneButton_isNotDisplayed_after_fragmentNewCanvasDoneButton_isPressed() {
+        createNewProject()
+        onView(withId(R.id.fragmentNewCanvas_doneButton)).check(ViewAssertions.doesNotExist())
+    }
+}
