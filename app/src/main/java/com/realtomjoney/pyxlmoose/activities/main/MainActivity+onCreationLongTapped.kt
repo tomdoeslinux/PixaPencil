f92baa455f68bb7815b2fee0c459b085db398262
xTheCoderx <justaguyoney@gmail.com>
2021-11-29 16:56:58 +1300

    Refactored code in MainActivity+onCreationLongTapped.kt to a separate 'restoreDeletedItem' function.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity+onCreationLongTapped.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity+onCreationLongTapped.kt
@@ -5,16 +5,19 @@ import com.realtomjoney.pyxlmoose.database.PixelArtDatabase
 import com.realtomjoney.pyxlmoose.extensions.SnackbarDuration
 import com.realtomjoney.pyxlmoose.extensions.showSnackbarWithAction
 
+fun MainActivity.restoreDeletedItem(param: PixelArt) {
+    PixelArtDatabase.addItem(param)
+    refreshAdapter()
+    binding.recentCreationsRecyclerView.adapter?.notifyItemInserted(
+        PixelArtDatabase.toList().indexOf((param)))
+}
+
 fun MainActivity.extendedOnCreationLongTapped(param: PixelArt) {
     PixelArtDatabase.removeItem(param)
     refreshAdapter()
     binding.recentCreationsRecyclerView.adapter?.notifyItemRemoved(PixelArtDatabase.toList().indexOf((param)))
 
-    (binding.recentCreationsRecyclerView)
-        .showSnackbarWithAction("You have deleted ${param.title}", SnackbarDuration.DEFAULT, "Undo") {
-            PixelArtDatabase.addItem(param)
-            refreshAdapter()
-            binding.recentCreationsRecyclerView.adapter?.notifyItemInserted(
-                PixelArtDatabase.toList().indexOf((param)))
-        }
+    (binding.recentCreationsRecyclerView).showSnackbarWithAction("You have deleted ${param.title}", SnackbarDuration.DEFAULT, "Undo") {
+        restoreDeletedItem(param)
+    }
 }
