0e21d8f2e153e84c91a902fa08aaf4c18750e15e
xTheCoderx <justaguyoney@gmail.com>
2021-11-30 21:53:14 +1300

    Removed unused import directives in CanvasActivityTestsOnFindAndReplaceButtonTap.kt.

--- app/src/androidTest/java/com/realtomjoney/pyxlmoose/activities/2_canvas/CanvasActivityTestsOnFindAndReplaceButtonTap.kt
+++ app/src/androidTest/java/com/realtomjoney/pyxlmoose/activities/2_canvas/CanvasActivityTestsOnFindAndReplaceButtonTap.kt
@@ -6,14 +6,12 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import com.realtomjoney.pyxlmoose.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.realtomjoney.pyxlmoose.activities.canvas.CanvasActivity
-import org.hamcrest.Matchers
 
 @LargeTest
 @RunWith(AndroidJUnit4ClassRunner::class)
