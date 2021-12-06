df08e201ffe425f78bb14a4fb86c829d9299a6be
xTheCoderx <justaguyoney@gmail.com>
2021-12-07 01:49:07 +1300

    Shortened code.

--- app/src/main/java/com/realtomjoney/pyxlmoose/converters/JsonConverter.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/converters/JsonConverter.kt
@@ -4,8 +4,6 @@ import com.google.gson.Gson
 import com.realtomjoney.pyxlmoose.models.Pixel
 
 object JsonConverter {
-    fun convertPixelListToJsonString(list: List<Pixel>): String {
-        return Gson().toJson(list)
-    }
+    fun convertPixelListToJsonString(list: List<Pixel>): String = Gson().toJson(list)
     fun convertJsonStringToPixelList(str: String) = Gson().fromJson(str, Array<Pixel>::class.java).toList()
 }
