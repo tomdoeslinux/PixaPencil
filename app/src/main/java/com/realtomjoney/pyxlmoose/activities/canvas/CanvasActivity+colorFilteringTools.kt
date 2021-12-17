7760521d39022d69b0b0e940718868cf6a1da170
xTheCoderx <justaguyoney@gmail.com>
2021-12-17 18:15:04 +1300

    Refactored color filtering methods to separate file.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+colorFilteringTools.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+colorFilteringTools.kt
@@ -23,3 +23,26 @@ private fun CanvasActivity.filterSelectedColor(color: Int, ratio: Float) {
 fun CanvasActivity.darkenSelectedColor() = filterSelectedColor(Color.BLACK, 0.2f)
 fun CanvasActivity.lightenSelectedColor() = filterSelectedColor(Color.WHITE, 0.2f)
 
+// Solution by Andrew Gallasch (https://stackoverflow.com/users/3841420/andrew-gallasch) on StackOverFlow (https://stackoverflow.com/questions/18141976/how-to-invert-an-rgb-color-in-integer-form)
+fun flipBits(n: Int) = n xor 0x00ffffff // TODO - Understand how this works a bit better
+
+fun flipRed(n: Int) = n xor 0x00ff0000 // TODO - Understand how this works a bit better
+fun flipGreen(n: Int) = n xor 0x0000ff00 // TODO - Understand how this works a bit better
+fun flipBlue(n: Int) = n xor 0x0000000ff // TODO - Understand how this works a bit better
+
+fun performCustomGrayscaleAlgorithm(n: Int, strength: Int = 10): Int {
+    // TODO - research whether this a good way to convert a color to grayscale?
+
+    var color = n
+
+    for (i in 0..strength) {
+        val r = Color.valueOf(color).red()
+        val g = Color.valueOf(color).green()
+        val b = Color.valueOf(color).blue()
+
+        color = (Color.rgb((r + g + b) / 3.toFloat(), r, r))
+    }
+
+    return color
+}
+
