644d59d29c1b03a28247848eebf6cae63d0380bf
xTheCoderx <justaguyoney@gmail.com>
2021-12-24 17:26:03 +1300

    Refactored Espresso code to use a library that is not deprecated.

--- app/src/androidTest/java/com/realtomjoney/pyxlmoose/activities/main/MainActivityTestsOnBackPressed.kt
+++ app/src/androidTest/java/com/realtomjoney/pyxlmoose/activities/main/MainActivityTestsOnBackPressed.kt
@@ -2,12 +2,11 @@ package com.realtomjoney.pyxlmoose.activities.main
 
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso
-import org.junit.Rule
-import org.junit.Test
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import junit.framework.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
@@ -19,6 +18,6 @@ class MainActivityTestsOnBackPressed {
     @Test
     fun uitest_activityMain_onBackPressed_lifecycleStateIsDestroyed() {
         Espresso.pressBackUnconditionally()
-        assertTrue(activityTestRule.scenario.state == Lifecycle.State.DESTROYED)
+        assert(activityTestRule.scenario.state == Lifecycle.State.DESTROYED)
     }
 }
