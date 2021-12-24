10c88b19cd968f57763edcdc750bf90a0d50404f
xTheCoderx <justaguyoney@gmail.com>
2021-12-24 16:54:25 +1300

    Removed unused import directives.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onBackPressed.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onBackPressed.kt
@@ -1,13 +1,7 @@
 package com.realtomjoney.pyxlmoose.activities.canvas
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.PorterDuff
-import androidx.core.view.drawToBitmap
 import com.realtomjoney.pyxlmoose.activities.main.MainActivity
-import com.realtomjoney.pyxlmoose.converters.BitmapConverter
-import com.realtomjoney.pyxlmoose.converters.JsonConverter
-import com.realtomjoney.pyxlmoose.database.AppData
 import com.realtomjoney.pyxlmoose.extensions.navigateHome
 import com.realtomjoney.pyxlmoose.extensions.showDialog
 import com.realtomjoney.pyxlmoose.utility.StringConstants
