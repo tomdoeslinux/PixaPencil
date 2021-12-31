87134ab8cda851588b5ab5af9047ebd215b89a6d
xTheCoderx <justaguyoney@gmail.com>
2022-01-01 01:01:31 +1300

    Fixed bug in which the primary and secondary color view were not showing properly.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+setPrimaryPixelColor.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+setPrimaryPixelColor.kt
@@ -1,5 +1,7 @@
 package com.realtomjoney.pyxlmoose.activities.canvas
 
+import android.util.Log
+
 fun setPrimaryPixelColor(color: Int) {
     primaryColor = color
     binding.activityCanvasColorPrimaryView.setBackgroundColor(color)
