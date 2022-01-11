32b26b2fb5f282d098dbd69092871ca4603eb5de
xTheCoderx <justaguyoney@gmail.com>
2022-01-11 14:47:06 +1300

    Removed unused functions in `MathExtensions.kt`.

--- app/src/main/java/com/realtomjoney/pyxlmoose/utility/MathExtensions.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/utility/MathExtensions.kt
@@ -7,22 +7,6 @@ object MathExtensions {
     fun reflectIndexHorizontally(index: Int, spanCount: Int) = ((((((ceil((index / spanCount).toDouble())) + spanCount) - (ceil((index / spanCount).toDouble()))) - (ceil((index / spanCount).toDouble()))) + (spanCount - 1) * (((ceil((index / spanCount).toDouble())) + spanCount) - (ceil((index / spanCount).toDouble())) - (ceil((index / spanCount).toDouble())))) + index.mod(spanCount) - spanCount).toInt()
     fun reflectIndexVertically(index: Int, spanCount: Int) = (index - (index.mod(spanCount))) + (spanCount - (index.mod(spanCount))) - 1
 
-    fun convertListOfSizeNToListOfXYPosition(listSize: Int, spanCount: Int): MutableList<XYPosition> {
-        val list = mutableListOf<XYPosition>()
-
-        for (i in 0 until listSize) list.add(convertIndexToXYPosition(i, spanCount))
-
-        return list
-    }
-
-    fun convertListToMap(listSize: Int, spanCount: Int): Map<XYPosition, Int> {
-        val map = mutableMapOf<XYPosition, Int>()
-
-        for (i in 0 until listSize) map[convertIndexToXYPosition(i, spanCount)] = i
-
-        return map
-    }
-
     fun convertXYPositionToIndex(xyPosition: XYPosition, spanCount: Int): Int {
         val positionX = xyPosition.x
         val positionY = xyPosition.y
