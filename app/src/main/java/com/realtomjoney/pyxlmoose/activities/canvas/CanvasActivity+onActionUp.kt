b144994906e0f5329cc24f1e757851e17e988e31
xTheCoderx <justaguyoney@gmail.com>
2022-01-21 01:55:23 +1300

    Made minor code style adjustment (2).

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onActionUp.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onActionUp.kt
@@ -1,5 +1,6 @@
 package com.realtomjoney.pyxlmoose.activities.canvas
 
+import com.realtomjoney.pyxlmoose.database.BrushesDatabase
 import com.realtomjoney.pyxlmoose.models.BitmapAction
 import com.realtomjoney.pyxlmoose.models.BitmapActionData
 
@@ -14,7 +15,9 @@ fun CanvasActivity.extendedOnActionUp() {
     } else {
         canvasInstance.myCanvasViewInstance.bitmapActionData.add(canvasInstance.myCanvasViewInstance.currentBitmapAction!!)
 
-        if (canvasInstance.myCanvasViewInstance.pixelPerfectMode && (currentTool == Tools.PENCIL_TOOL || currentTool == Tools.VERTICAL_MIRROR_TOOL || currentTool == Tools.HORIZONTAL_MIRROR_TOOL || currentTool == Tools.ERASE_TOOL)) {
+        if (canvasInstance.myCanvasViewInstance.pixelPerfectMode
+            && (currentTool == Tools.PENCIL_TOOL || currentTool == Tools.VERTICAL_MIRROR_TOOL || currentTool == Tools.HORIZONTAL_MIRROR_TOOL || currentTool == Tools.ERASE_TOOL)
+            && (canvasInstance.myCanvasViewInstance.currentBrush == null || canvasInstance.myCanvasViewInstance.currentBrush == BrushesDatabase.toList().first())) {
 
             // Thanks to https://rickyhan.com/jekyll/update/2018/11/22/pixel-art-algorithm-pixel-perfect.html
 
