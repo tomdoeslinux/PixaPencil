e36cc2f1ca6c80516b111b562eab5fc517a40891
xTheCoderx <justaguyoney@gmail.com>
2022-01-24 18:00:36 +1300

    Fixed bug in which the wrong title was being set.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity+setTitle.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity+setTitle.kt
@@ -1,5 +1,7 @@
 package com.realtomjoney.pyxlmoose.activities.main
 
+import com.realtomjoney.pyxlmoose.utility.StringConstants
+
 fun MainActivity.setTitle() {
-    title = "Home"
+    title = StringConstants.APP_NAME
 }
