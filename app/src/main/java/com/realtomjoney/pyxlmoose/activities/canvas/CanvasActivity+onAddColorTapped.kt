3223a8a9e63f7af7654889bb4bb925db70720629
thebluepandabear <justaguyoney@gmail.com>
2022-01-31 01:56:45 +1300

    Moved lambda arguments out of parentheses (2).

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onAddColorTapped.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onAddColorTapped.kt
@@ -8,13 +8,13 @@ var fromDB: ColorPalette? = null
 fun CanvasActivity.extendedOnAddColorTapped(colorPalette: ColorPalette) {
     hideMenuItems()
 
-    AppData.colorPalettesDB.colorPalettesDao().getAllColorPalettes().observe(this, {
+    AppData.colorPalettesDB.colorPalettesDao().getAllColorPalettes().observe(this) {
         for (data in it) {
             if (data.objId == colorPalette.objId) {
                 fromDB = data
                 break
             }
         }
-    })
+    }
     openColorPickerDialog(true)
 }
