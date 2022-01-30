0ccc18ecc64599385111f11a3f7553aaa3bd8232
thebluepandabear <justaguyoney@gmail.com>
2022-01-31 01:56:13 +1300

    Fixed bug in which the menu items were still showing when adding a new color to the user's color palette.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onAddColorTapped.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onAddColorTapped.kt
@@ -6,6 +6,8 @@ import com.realtomjoney.pyxlmoose.models.ColorPalette
 var fromDB: ColorPalette? = null
 
 fun CanvasActivity.extendedOnAddColorTapped(colorPalette: ColorPalette) {
+    hideMenuItems()
+
     AppData.colorPalettesDB.colorPalettesDao().getAllColorPalettes().observe(this, {
         for (data in it) {
             if (data.objId == colorPalette.objId) {
