73e2c48c0e76a9c8a555407ad045e67391fae5c8
thebluepandabear <justaguyoney@gmail.com>
2022-01-30 16:00:26 +1300

    Moved lambda arguments out of parentheses.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onCreate.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onCreate.kt
@@ -19,10 +19,10 @@ fun CanvasActivity.onCreate() {
     initSharedPreferenceObject()
 
     if (index != -1) {
-        AppData.pixelArtDB.pixelArtCreationsDao().getAllPixelArtCreations().observe(context, {
+        AppData.pixelArtDB.pixelArtCreationsDao().getAllPixelArtCreations().observe(context) {
             currentPixelArtObj = it[index!!]
             outerCanvasInstance.canvasFragment.myCanvasViewInstance.replaceBitmap(BitmapConverter.convertStringToBitmap(currentPixelArtObj.bitmap)!!)
-        })
+        }
     }
 
     title = (intent.getStringExtra(StringConstants.PROJECT_TITLE_EXTRA))
