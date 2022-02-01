ad305929e9eb1708a92c46c7b27abb2375ecfced
thebluepandabear <justaguyoney@gmail.com>
2022-02-01 18:38:17 +1300

    Cleaned up code in `CanvasActivity.onCreate()`.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onCreate.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onCreate.kt
@@ -1,9 +1,7 @@
 package com.realtomjoney.pyxlmoose.activities.canvas
 
-import com.realtomjoney.pyxlmoose.converters.BitmapConverter
 import com.realtomjoney.pyxlmoose.database.AppData
 import com.realtomjoney.pyxlmoose.database.ColorPalettesDatabase
-import com.realtomjoney.pyxlmoose.utility.StringConstants
 
 fun CanvasActivity.onCreate() {
     getExtras()
@@ -15,11 +13,5 @@ fun CanvasActivity.onCreate() {
     setOnClickListeners()
     setColors()
     initSharedPreferenceObject()
-
-    if (index != -1) {
-        AppData.pixelArtDB.pixelArtCreationsDao().getAllPixelArtCreations().observe(context) {
-            currentPixelArtObj = it[index!!]
-            outerCanvasInstance.canvasFragment.myCanvasViewInstance.replaceBitmap(BitmapConverter.convertStringToBitmap(currentPixelArtObj.bitmap)!!)
-        }
-    }
+    replaceBitmapIfApplicable()
 }
---
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+replaceBitmapIfApplicable.kt
@@ -0,0 +1,13 @@
+package com.realtomjoney.pyxlmoose.activities.canvas
+
+import com.realtomjoney.pyxlmoose.converters.BitmapConverter
+import com.realtomjoney.pyxlmoose.database.AppData
+
+fun CanvasActivity.replaceBitmapIfApplicable() {
+    if (index != -1) {
+        AppData.pixelArtDB.pixelArtCreationsDao().getAllPixelArtCreations().observe(context) {
+            currentPixelArtObj = it[index!!]
+            outerCanvasInstance.canvasFragment.myCanvasViewInstance.replaceBitmap(BitmapConverter.convertStringToBitmap(currentPixelArtObj.bitmap)!!)
+        }
+    }
+}
