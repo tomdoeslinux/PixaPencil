95df830be654f2f8a1005a673eb841359300e756
thebluepandabear <justaguyoney@gmail.com>
2022-02-01 18:53:40 +1300

    Cleaned up code in `NewCanvasFragment+setOnClickListeners.kt` by utilizing '.apply { }'.

--- app/src/main/java/com/realtomjoney/pyxlmoose/fragments/newcanvas/NewCanvasFragment+setOnClickListeners.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/fragments/newcanvas/NewCanvasFragment+setOnClickListeners.kt
@@ -1,28 +1,33 @@
 package com.realtomjoney.pyxlmoose.fragments.newcanvas
 
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.realtomjoney.pyxlmoose.extensions.SnackbarDuration
 import com.realtomjoney.pyxlmoose.extensions.showSnackbar
 import com.realtomjoney.pyxlmoose.utility.StringConstants
 
-fun NewCanvasFragment.setOnClickListeners() {
-    binding.fragmentNewCanvasDoneButton.setOnClickListener {
-        val parsedSpanCountValue =  Integer.parseInt(binding.fragmentNewCanvasSpanCountTextInputEditText.text.toString())
+fun setOnClickListeners() {
+    binding.apply {
+        fragmentNewCanvasDoneButton.setOnClickListener {
+            val parsedSpanCountValue =
+                Integer.parseInt(fragmentNewCanvasSpanCountTextInputEditText.text.toString())
 
-        if (parsedSpanCountValue <= 0) {
-            (root as ConstraintLayout).showSnackbar(StringConstants.EX_INVALID_SPAN_COUNT, SnackbarDuration.DEFAULT)
-        } else {
-            try {
-                caller.onDoneButtonPressed(
-                    Integer.parseInt(binding.fragmentNewCanvasSpanCountTextInputEditText.text.toString()),
-                    binding.fragmentNewCanvasSpanCountTextInputEditText,
-                    binding.fragmentNewCanvasProjectTitleTextInputEditText
-                )
-            } catch (ex: Exception) {
-                (root as ConstraintLayout).showSnackbar(
+            if (parsedSpanCountValue <= 0) {
+                root.showSnackbar(
                     StringConstants.EX_INVALID_SPAN_COUNT,
                     SnackbarDuration.DEFAULT
                 )
+            } else {
+                try {
+                    caller.onDoneButtonPressed(
+                        Integer.parseInt(fragmentNewCanvasSpanCountTextInputEditText.text.toString()),
+                        fragmentNewCanvasSpanCountTextInputEditText,
+                        fragmentNewCanvasProjectTitleTextInputEditText
+                    )
+                } catch (ex: Exception) {
+                    root.showSnackbar(
+                        StringConstants.EX_INVALID_SPAN_COUNT,
+                        SnackbarDuration.DEFAULT
+                    )
+                }
             }
         }
     }
