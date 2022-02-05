453af78d69c9c514455e0dff38648ef3dd33881b
thebluepandabear <justaguyoney@gmail.com>
2022-02-06 03:15:11 +1300

    Removed redundant database access code.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onCreate.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onCreate.kt
@@ -1,12 +1,7 @@
 package com.realtomjoney.pyxlmoose.activities.canvas
 
-import com.realtomjoney.pyxlmoose.database.AppData
-import com.realtomjoney.pyxlmoose.database.ColorPalettesDatabase
-
 fun CanvasActivity.onCreate() {
     getExtras()
-    AppData.colorPalettesDB = ColorPalettesDatabase.getDatabase(this)
-
     setUpFragment()
     setBindings()
     setUpRecyclerView()
--- app/src/main/java/com/realtomjoney/pyxlmoose/algorithms/AlgorithmInfoParameter.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/algorithms/AlgorithmInfoParameter.kt
@@ -2,6 +2,5 @@ package com.realtomjoney.pyxlmoose.algorithms
 
 import android.graphics.Bitmap
 import com.realtomjoney.pyxlmoose.models.BitmapAction
-import com.realtomjoney.pyxlmoose.models.BitmapActionData
 
 class AlgorithmInfoParameter(val bitmap: Bitmap, val currentBitmapAction: BitmapAction, var color: Int, val currentBitmapActionData: List<BitmapAction>? = null)
