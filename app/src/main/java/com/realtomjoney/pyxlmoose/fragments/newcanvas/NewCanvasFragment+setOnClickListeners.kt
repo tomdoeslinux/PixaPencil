d2aaf8726f3d90eca67444055d89f63157efb1f8
thebluepandabear <justaguyoney@gmail.com>
2022-02-10 14:22:29 +1300

    Moved all constant integer values to `IntConstants.kt`.

--- app/src/main/java/com/realtomjoney/pyxlmoose/fragments/newcanvas/NewCanvasFragment+setOnClickListeners.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/fragments/newcanvas/NewCanvasFragment+setOnClickListeners.kt
@@ -2,10 +2,8 @@ package com.realtomjoney.pyxlmoose.fragments.newcanvas
 
 import com.realtomjoney.pyxlmoose.extensions.SnackbarDuration
 import com.realtomjoney.pyxlmoose.extensions.showSnackbar
+import com.realtomjoney.pyxlmoose.utility.IntConstants
 import com.realtomjoney.pyxlmoose.utility.StringConstants
-
-const val SPAN_COUNT_MIN = 1
-const val SPAN_COUNT_MAX = 10000
 
 fun setOnClickListeners() {
     binding.apply {
@@ -13,7 +11,7 @@ fun setOnClickListeners() {
             val parsedSpanCountValue =
                 Integer.parseInt(fragmentNewCanvasSpanCountTextInputEditText.text.toString())
 
-            if (parsedSpanCountValue !in SPAN_COUNT_MIN..SPAN_COUNT_MAX) {
+            if (parsedSpanCountValue !in IntConstants.SPAN_COUNT_MIN..IntConstants.SPAN_COUNT_MAX) {
                 root.showSnackbar(
                     StringConstants.EX_INVALID_SPAN_COUNT,
                     SnackbarDuration.DEFAULT
---
+++ app/src/main/java/com/realtomjoney/pyxlmoose/utility/IntConstants.kt
@@ -0,0 +1,6 @@
+package com.realtomjoney.pyxlmoose.utility
+
+object IntConstants {
+    const val SPAN_COUNT_MIN = 1
+    const val SPAN_COUNT_MAX = 10000
+}
