5a25c56ac116a42f8c6f11f7a4178c31e8c5929e
thebluepandabear <justaguyoney@gmail.com>
2022-02-10 14:17:43 +1300

    Added `SPAN_COUNT_MIN` and `SPAN_COUNT_MAX` values.

--- app/src/main/java/com/realtomjoney/pyxlmoose/fragments/newcanvas/NewCanvasFragment+setOnClickListeners.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/fragments/newcanvas/NewCanvasFragment+setOnClickListeners.kt
@@ -4,13 +4,16 @@ import com.realtomjoney.pyxlmoose.extensions.SnackbarDuration
 import com.realtomjoney.pyxlmoose.extensions.showSnackbar
 import com.realtomjoney.pyxlmoose.utility.StringConstants
 
+const val SPAN_COUNT_MIN = 1
+const val SPAN_COUNT_MAX = 10000
+
 fun setOnClickListeners() {
     binding.apply {
         fragmentNewCanvasDoneButton.setOnClickListener {
             val parsedSpanCountValue =
                 Integer.parseInt(fragmentNewCanvasSpanCountTextInputEditText.text.toString())
 
-            if (parsedSpanCountValue <= 0) {
+            if (parsedSpanCountValue !in SPAN_COUNT_MIN..SPAN_COUNT_MAX) {
                 root.showSnackbar(
                     StringConstants.EX_INVALID_SPAN_COUNT,
                     SnackbarDuration.DEFAULT
