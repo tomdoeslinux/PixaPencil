76d6bf2c12d73c1044b0bc7d1533b603b86a0293
thebluepandabear <justaguyoney@gmail.com>
2022-02-10 14:32:56 +1300

    The user will now notice/sense some haptic feedback if they input an invalid project title or span count.

--- app/src/main/java/com/realtomjoney/pyxlmoose/fragments/newcanvas/NewCanvasFragment+setOnClickListeners.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/fragments/newcanvas/NewCanvasFragment+setOnClickListeners.kt
@@ -1,9 +1,18 @@
 package com.realtomjoney.pyxlmoose.fragments.newcanvas
 
+import android.view.HapticFeedbackConstants
+import android.view.View
 import com.realtomjoney.pyxlmoose.extensions.SnackbarDuration
 import com.realtomjoney.pyxlmoose.extensions.showSnackbar
 import com.realtomjoney.pyxlmoose.utility.IntConstants
 import com.realtomjoney.pyxlmoose.utility.StringConstants
+
+fun performHapticFeedbackOn(view: View) {
+    view.performHapticFeedback(
+        HapticFeedbackConstants.VIRTUAL_KEY,
+        HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING
+    )
+}
 
 fun setOnClickListeners() {
     binding.apply {
@@ -12,6 +21,7 @@ fun setOnClickListeners() {
                 Integer.parseInt(fragmentNewCanvasSpanCountTextInputEditText.text.toString())
 
             if (parsedSpanCountValue !in IntConstants.SPAN_COUNT_MIN..IntConstants.SPAN_COUNT_MAX) {
+                performHapticFeedbackOn(binding.fragmentNewCanvasDoneButton)
                 root.showSnackbar(
                     StringConstants.EX_INVALID_SPAN_COUNT,
                     SnackbarDuration.DEFAULT
@@ -24,6 +34,7 @@ fun setOnClickListeners() {
                         fragmentNewCanvasProjectTitleTextInputEditText
                     )
                 } catch (ex: Exception) {
+                    performHapticFeedbackOn(binding.fragmentNewCanvasDoneButton)
                     root.showSnackbar(
                         StringConstants.EX_INVALID_SPAN_COUNT,
                         SnackbarDuration.DEFAULT
