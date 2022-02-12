f98018bed27d915da52cb020bec06a09d4fa648b
thebluepandabear <justaguyoney@gmail.com>
2022-02-12 14:57:24 +1300

    Fixed minor bug.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+ColorPickerFragment+onDoneButtonPressed.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+ColorPickerFragment+onDoneButtonPressed.kt
@@ -1,6 +1,7 @@
 package com.realtomjoney.pyxlmoose.activities.canvas
 
 import android.graphics.Color
+import android.view.View
 import com.realtomjoney.pyxlmoose.adapters.ColorPickerAdapter
 import com.realtomjoney.pyxlmoose.converters.JsonConverter
 import com.realtomjoney.pyxlmoose.database.AppData
@@ -32,4 +33,12 @@ fun CanvasActivity.extendedOnDoneButtonPressed(selectedColor: Int, colorPaletteM
     currentFragmentInstance = null
     navigateHome(supportFragmentManager, colorPickerFragmentInstance, binding.activityCanvasRootLayout, binding.activityCanvasPrimaryFragmentHost, intent.getStringExtra(
         StringConstants.PROJECT_TITLE_EXTRA)!!)
+
+    if (isPrimaryColorSelected) {
+        binding.activityCanvasColorPrimaryViewIndicator.visibility = View.VISIBLE
+        binding.activityCanvasColorSecondaryViewIndicator.visibility = View.INVISIBLE
+    } else {
+        binding.activityCanvasColorPrimaryViewIndicator.visibility = View.INVISIBLE
+        binding.activityCanvasColorSecondaryViewIndicator.visibility = View.VISIBLE
+    }
 }
