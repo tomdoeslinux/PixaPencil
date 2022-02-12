807fe7c0bff3235d5dddebcf55bfb00959240ebf
thebluepandabear <justaguyoney@gmail.com>
2022-02-12 18:55:48 +1300

    Added improvement #49 (resolve #49).

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity+onCreationLongTapped.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/main/MainActivity+onCreationLongTapped.kt
@@ -2,13 +2,32 @@ package com.realtomjoney.pyxlmoose.activities.main
 
 import com.realtomjoney.pyxlmoose.adapters.RecentCreationsAdapter
 import com.realtomjoney.pyxlmoose.database.AppData
+import com.realtomjoney.pyxlmoose.extensions.showDialog
 import com.realtomjoney.pyxlmoose.models.PixelArt
+import com.realtomjoney.pyxlmoose.utility.StringConstants
 
 fun MainActivity.extendedOnCreationLongTapped(param: PixelArt) {
-    (binding.activityMainRecentCreationsRecyclerView.adapter as RecentCreationsAdapter).userHasLongPressed = true
+    (binding.activityMainRecentCreationsRecyclerView.adapter as RecentCreationsAdapter).userHasLongPressed =
+        true
 
-    AppData.pixelArtDB.pixelArtCreationsDao().getAllPixelArtCreations().observe(this) {
-        AppData.pixelArtDB.pixelArtCreationsDao().deletePixelArtCreation(param.objId)
-        binding.activityMainRecentCreationsRecyclerView.adapter!!.notifyItemRemoved(it.indexOf(param))
+    val paramTitle = param.title
+
+    if ((binding.activityMainRecentCreationsRecyclerView.adapter as RecentCreationsAdapter).userHasLongPressed) {
+        showDialog(
+            "Delete '$paramTitle'?",
+            "Are you sure you want to delete '$paramTitle'? - this cannot be undone.",
+            StringConstants.DIALOG_POSITIVE_BUTTON_TEXT, { _, _ ->
+                AppData.pixelArtDB.pixelArtCreationsDao().getAllPixelArtCreations().observe(this) {
+
+                    AppData.pixelArtDB.pixelArtCreationsDao()
+                        .deletePixelArtCreation(param.objId)
+                    binding.activityMainRecentCreationsRecyclerView.adapter!!.notifyItemRemoved(
+                        it.indexOf(
+                            param
+                        )
+                    )
+                }
+            }, "Cancel", null, null
+        )
     }
 }
