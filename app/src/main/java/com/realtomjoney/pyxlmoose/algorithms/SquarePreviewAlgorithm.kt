3239dce397e26a0e5f3842267be017b9c2167934
thebluepandabear <justaguyoney@gmail.com>
2022-02-22 02:31:06 +1300

    Cleaned up code.

--- app/src/main/java/com/realtomjoney/pyxlmoose/algorithms/SquarePreviewAlgorithm.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/algorithms/SquarePreviewAlgorithm.kt
@@ -3,67 +3,78 @@ package com.realtomjoney.pyxlmoose.algorithms
 import com.realtomjoney.pyxlmoose.activities.canvas.coordinates
 import com.realtomjoney.pyxlmoose.models.Coordinates
 
-class SquarePreviewAlgorithm(private val algorithmInfo: AlgorithmInfoParameter, private var endCoords: Coordinates? = null) {
+class SquarePreviewAlgorithm(private val algorithmInfo: AlgorithmInfoParameter, private var endCoordinates: Coordinates? = null) {
+    private fun setsqc1(lineAlgorithmInstance: LineAlgorithm, from: Coordinates, size: Int) {
+        lineAlgorithmInstance.apply {
+            compute(Coordinates(from.x, from.y), Coordinates(from.x + size, from.y))
+            compute(Coordinates(from.x, from.y), Coordinates(from.x, from.y + size))
+            compute(Coordinates(from.x, from.y + size), Coordinates(from.x + size, from.y + size))
+            compute(Coordinates(from.x + size, from.y + size), Coordinates(from.x + size, from.y))
+        }
+
+        endCoordinates = Coordinates(from.x + size, from.y + size)
+    }
+
+    private fun setsqc2(lineAlgorithmInstance: LineAlgorithm, from: Coordinates, size: Int) {
+        lineAlgorithmInstance.apply {
+            compute(Coordinates(from.x, from.y), Coordinates(from.x, from.y + size))
+            compute(Coordinates(from.x, from.y), Coordinates(from.x + size, from.y))
+            compute(Coordinates(from.x + size, from.y), Coordinates(from.x + size, from.y + size))
+            compute(Coordinates(from.x + size, from.y + size), Coordinates(from.x, from.y + size))
+        }
+
+        endCoordinates = Coordinates(from.x + size, from.y + size)
+    }
+
+    private fun setsqc3(lineAlgorithmInstance: LineAlgorithm, from: Coordinates, size: Int) {
+        lineAlgorithmInstance.apply {
+            compute(Coordinates(from.x, from.y), Coordinates(from.x - size, from.y))
+            compute(Coordinates(from.x - size, from.y), Coordinates(from.x - size, from.y + size))
+            compute(Coordinates(from.x - size, from.y + size), Coordinates(from.x, from.y + size))
+            compute(Coordinates(from.x, from.y + size), Coordinates(from.x, from.y))
+        }
+
+        endCoordinates = Coordinates(from.x - size, from.y + size)
+    }
+
+    private fun setsqc4(lineAlgorithmInstance: LineAlgorithm, from: Coordinates, to: Coordinates) {
+
+        val size = (from.x - to.x)
+
+        lineAlgorithmInstance.apply {
+            compute(Coordinates(from.x, from.y), Coordinates(from.x, from.y + size))
+            compute(Coordinates(from.x, from.y + size), Coordinates(from.x - size, from.y + size))
+            compute(Coordinates(from.x - size, from.y + size), Coordinates(from.x - size, from.y))
+            compute(Coordinates(from.x - size, from.y), Coordinates(from.x, from.y))
+        }
+
+        endCoordinates = Coordinates(from.x - size, from.y + size)
+    }
+
     fun compute(from: Coordinates, to: Coordinates) {
-        val modifiedShapeAlgorithmInfo = algorithmInfo
-        modifiedShapeAlgorithmInfo.color = algorithmInfo.color
+        val lineAlgorithmInstance = LineAlgorithm(algorithmInfo, true)
 
-        val lineAlgorithmInstance = LineAlgorithm(modifiedShapeAlgorithmInfo, true)
+        val fromDouble = from.convertToCoordinatesDouble()
+        val toDouble = to.convertToCoordinatesDouble()
 
-        val gradient: Double = (from.y.toDouble() - to.y.toDouble()) / (from.x.toDouble() - to.x.toDouble())
+        val gradient: Double = (fromDouble.y - toDouble.y) / (fromDouble.x - toDouble.x)
 
-        var dimen = if (gradient > 1.0) to.x - from.x else to.y - from.y
+        val size = if (gradient > 1.0) {
+            to.x - from.x
+        } else {
+            to.y - from.y
+        }
 
         if (gradient in 0.0..1.0) {
-            lineAlgorithmInstance.apply {
-                compute(Coordinates(from.x, from.y), Coordinates(from.x + dimen, from.y))
-                compute(Coordinates(from.x, from.y), Coordinates(from.x, from.y + dimen))
-                compute(
-                    Coordinates(from.x, from.y + dimen),
-                    Coordinates(from.x + dimen, from.y + dimen)
-                )
-                compute(
-                    Coordinates(from.x + dimen, from.y + dimen),
-                    Coordinates(from.x + dimen, from.y)
-                )
-            }
-
-            endCoords = Coordinates(from.x + dimen, from.y + dimen)
+            setsqc1(lineAlgorithmInstance, from, size)
         } else if (gradient > 1.0 && gradient > 0){
-            lineAlgorithmInstance.apply {
-                compute(Coordinates(from.x, from.y), Coordinates(from.x, from.y + dimen))
-                compute(Coordinates(from.x, from.y), Coordinates(from.x + dimen, from.y))
-                compute(
-                    Coordinates(from.x + dimen, from.y),
-                    Coordinates(from.x + dimen, from.y + dimen)
-                )
-                compute(
-                    Coordinates(from.x + dimen, from.y + dimen),
-                    Coordinates(from.x, from.y + dimen)
-                )
-            }
-
-            endCoords = Coordinates(from.x + dimen, from.y + dimen)
+            setsqc2(lineAlgorithmInstance, from, size)
         } else if (gradient < 0 && gradient > -1.0) {
-            lineAlgorithmInstance.apply {
-                compute(Coordinates(from.x, from.y), Coordinates(from.x - dimen, from.y))
-                compute(Coordinates(from.x - dimen, from.y), Coordinates(from.x - dimen, from.y + dimen))
-                compute(Coordinates(from.x - dimen, from.y + dimen), Coordinates(from.x, from.y + dimen))
-                compute(Coordinates(from.x, from.y + dimen), Coordinates(from.x, from.y))
-            }
-
-            endCoords = Coordinates(from.x - dimen, from.y + dimen)
+            setsqc3(lineAlgorithmInstance, from, size)
         } else {
-            dimen = (from.x - to.x)
-            lineAlgorithmInstance.apply {
-                compute(Coordinates(from.x, from.y), Coordinates(from.x, from.y + dimen))
-                compute(Coordinates(from.x, from.y + dimen), Coordinates(from.x - dimen, from.y + dimen))
-                compute(Coordinates(from.x - dimen, from.y + dimen), Coordinates(from.x - dimen, from.y))
-                compute(Coordinates(from.x - dimen, from.y), Coordinates(from.x, from.y))
-            }
-
-            endCoords = Coordinates(from.x - dimen, from.y + dimen)
+            setsqc4(lineAlgorithmInstance, from, to)
         }
-        coordinates = endCoords
+
+        coordinates = endCoordinates
     }
 }
--- app/src/main/java/com/realtomjoney/pyxlmoose/models/Coordinates.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/models/Coordinates.kt
@@ -1,3 +1,9 @@
 package com.realtomjoney.pyxlmoose.models
 
-data class Coordinates(val x: Int, val y: Int)
+data class Coordinates(val x: Int, val y: Int) {
+    inner class CoordinatesDouble(val x: Double, val y: Double)
+
+    fun convertToCoordinatesDouble(): CoordinatesDouble {
+        return CoordinatesDouble(x.toDouble(), y.toDouble())
+    }
+}
