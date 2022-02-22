c02e27077784ed494487f9b072fd998e10bec204
thebluepandabear <justaguyoney@gmail.com>
2022-02-22 17:21:11 +1300

    Cleaned up code.

---
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+onColorLongTapped.kt
@@ -0,0 +1,27 @@
+package com.realtomjoney.pyxlmoose.activities.canvas
+
+import com.realtomjoney.pyxlmoose.converters.JsonConverter
+import com.realtomjoney.pyxlmoose.database.AppData
+import com.realtomjoney.pyxlmoose.extensions.SnackbarDuration
+import com.realtomjoney.pyxlmoose.extensions.showSnackbarWithAction
+import com.realtomjoney.pyxlmoose.models.ColorPalette
+
+fun extendedOnColorLongTapped(colorPalette: ColorPalette, colorIndex: Int) {
+    val extractedJson =
+        JsonConverter.convertJsonStringToListOfInt(colorPalette.colorPaletteColorData).toMutableList()
+    extractedJson.removeAt(colorIndex)
+
+    val color = extractedJson[colorIndex]
+
+    AppData.colorPalettesDB.colorPalettesDao().updateColorPaletteColorData(
+        JsonConverter.convertListOfIntToJsonString(extractedJson), colorPalette.objId)
+
+    val colorPaletteName = colorPalette.colorPaletteName
+
+    binding.activityCanvasRootLayout.showSnackbarWithAction("Removed color from '$colorPaletteName'", SnackbarDuration.DEFAULT, "Undo") {
+        extractedJson.add(colorIndex, color)
+
+        AppData.colorPalettesDB.colorPalettesDao().updateColorPaletteColorData(
+            JsonConverter.convertListOfIntToJsonString(extractedJson), colorPalette.objId)
+    }
+}
