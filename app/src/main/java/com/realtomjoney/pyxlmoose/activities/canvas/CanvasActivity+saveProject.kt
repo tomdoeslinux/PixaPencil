f39eb92cd2ac767e49c85b563cd76e64a127e5bf
thebluepandabear <justaguyoney@gmail.com>
2022-02-28 23:25:11 +1300

    Removed redundant code.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+saveProject.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+saveProject.kt
@@ -11,8 +11,6 @@ import kotlinx.coroutines.launch
 fun CanvasActivity.extendedSaveProject() {
     saved = true
 
-    outerCanvasInstance.canvasFragment.myCanvasViewInstance.hideGrid()
-
     val bmp = getCoverImageBitmap()
 
     if (index == -1) {
