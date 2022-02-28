d475d34a266da9fa5443c5b5b6443683592eef66
thebluepandabear <justaguyoney@gmail.com>
2022-02-28 21:55:43 +1300

    Fixed bug in which the pixel grid was showing in the 'Find and Replace' preview.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+getCoverImageBitmap.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+getCoverImageBitmap.kt
@@ -4,7 +4,14 @@ import android.graphics.Bitmap
 import android.graphics.Matrix
 import androidx.core.view.drawToBitmap
 
+var gridWasEnabled = false
+
 fun getCoverImageBitmap(): Bitmap {
+    if (outerCanvasInstance.canvasFragment.myCanvasViewInstance.gridEnabled) {
+        outerCanvasInstance.canvasFragment.myCanvasViewInstance.hideGrid()
+        gridWasEnabled = true
+    }
+
     val bmp = outerCanvasInstance.fragmentHost.drawToBitmap()
     val bmps: Bitmap?
 
@@ -12,6 +19,10 @@ fun getCoverImageBitmap(): Bitmap {
     matrix.setRotate(outerCanvasInstance.getCurrentRotation())
 
     bmps = Bitmap.createBitmap(bmp, 0, 0, bmp.width, bmp.width, matrix, false)
+
+    if (gridWasEnabled) {
+        outerCanvasInstance.canvasFragment.myCanvasViewInstance.showGrid()
+    }
 
     return bmps
 }
