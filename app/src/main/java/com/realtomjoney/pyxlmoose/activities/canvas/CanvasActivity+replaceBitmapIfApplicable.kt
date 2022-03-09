7f5db2dc6203856e129a8bcf33dc9e15697e2344
thebluepandabear <justaguyoney@gmail.com>
2022-03-09 15:32:28 +1300

    Cleaned up code.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+replaceBitmapIfApplicable.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+replaceBitmapIfApplicable.kt
@@ -4,13 +4,3 @@ import com.realtomjoney.pyxlmoose.converters.BitmapConverter
 import com.realtomjoney.pyxlmoose.database.AppData
 import com.realtomjoney.pyxlmoose.utility.IntConstants
 
-fun CanvasActivity.replaceBitmapIfApplicable() {
-    if (index != -1) {
-        AppData.pixelArtDB.pixelArtCreationsDao().getAllPixelArtCreations().observe(context) {
-            currentPixelArtObj = it[index!!]
-            outerCanvasInstance.canvasFragment.myCanvasViewInstance.replaceBitmap(BitmapConverter.convertStringToBitmap(currentPixelArtObj.bitmap)!!)
-            outerCanvasInstance.rotate(it[index!!].rotation.toInt(), false)
-            IntConstants.SPAN_COUNT = currentPixelArtObj.width
-        }
-    }
-}
--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+variables.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+variables.kt
@@ -61,7 +61,6 @@ var saved = true
 
 lateinit var colorPickerFragmentInstance: ColorPickerFragment
 lateinit var outerCanvasInstance: OuterCanvasFragment
-var itint = ::outerCanvasInstance.isInitialized
 lateinit var findAndReplaceFragmentInstance: FindAndReplaceFragment
 lateinit var newColorPaletteFragmentInstance: NewColorPaletteFragment
 lateinit var sprayToolSettingsFragmentInstance: SprayToolSettingsFragment
