1bc6f1677eeafdb971a5d678838a50ffaafa5a33
thebluepandabear <justaguyoney@gmail.com>
2022-03-21 15:41:37 +1300

    Cleaned up code (19) - more specifically removed deprecated 'ORIENTATION_SQUARE' from the `getScreenOrientation()` function.

--- app/src/main/java/com/realtomjoney/pyxlmoose/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/extensions/ActivityExtensions.kt
@@ -1,3 +1,4 @@
+
 package com.realtomjoney.pyxlmoose.extensions
 
 import android.app.Activity
@@ -7,14 +8,11 @@ import android.view.Display
 fun Activity.getScreenOrientation(): Int {
     val getOrient: Display = windowManager.defaultDisplay
 
-    val orientation: Int = if (getOrient.width === getOrient.height) {
-        Configuration.ORIENTATION_SQUARE
-    } else {
-        if (getOrient.width < getOrient.height) {
+    val orientation: Int = if (getOrient.width < getOrient.height) {
             Configuration.ORIENTATION_PORTRAIT
         } else {
             Configuration.ORIENTATION_LANDSCAPE
         }
-    }
+
     return orientation
 }
