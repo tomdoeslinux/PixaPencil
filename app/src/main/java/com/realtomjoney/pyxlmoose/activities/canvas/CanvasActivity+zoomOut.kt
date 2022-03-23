e71eb7c4d371951dda7e596669e1281b94b3f305
thebluepandabear <justaguyoney@gmail.com>
2022-03-23 15:40:46 +1300

    Zoom out toolbar item will be grayed out/disabled if the user has reached the maximum 'zoom out' level.

--- app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+zoomOut.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/activities/canvas/CanvasActivity+zoomOut.kt
@@ -1,5 +1,9 @@
 package com.realtomjoney.pyxlmoose.activities.canvas
 
+import com.realtomjoney.pyxlmoose.R
+import com.realtomjoney.pyxlmoose.extensions.disable
+import java.math.RoundingMode
+
 fun zoomOut() {
     outerCanvasInstance.cardViewParent.apply {
         val canZoomOut = outerCanvasInstance.cardViewParent.scaleX - zoomIncrement > zoomIncrement
@@ -7,6 +11,10 @@ fun zoomOut() {
         if (canZoomOut) {
             scaleX -= zoomIncrement
             scaleY -= zoomIncrement
+        }
+
+        if (scaleX - zoomIncrement < zoomIncrement.toBigDecimal().setScale(1, RoundingMode.UP).toDouble()) {
+            menu.findItem(R.id.zoom_out).disable()
         }
     }
 
