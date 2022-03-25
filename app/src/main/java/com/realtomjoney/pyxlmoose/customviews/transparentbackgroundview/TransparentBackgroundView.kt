f10128ae7c718235102c25d5c8b9fffda9a6d382
thebluepandabear <justaguyoney@gmail.com>
2022-03-25 15:55:06 +1300

    Cleaned up code (9).

--- app/src/main/java/com/realtomjoney/pyxlmoose/customviews/transparentbackgroundview/TransparentBackgroundView.kt
+++ app/src/main/java/com/realtomjoney/pyxlmoose/customviews/transparentbackgroundview/TransparentBackgroundView.kt
@@ -6,7 +6,6 @@ import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.Matrix
 import android.view.View
 import com.realtomjoney.pyxlmoose.activities.canvas.binding
 import com.realtomjoney.pyxlmoose.activities.canvas.currentPixelArtObj
