d8a132bf5e112ad7f5cff48430441860b4cc1973
thebluepandabear <justaguyoney@gmail.com>
2022-04-06 17:45:49 +1200

    Fixed minor bug.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+onResume.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+onResume.kt
@@ -3,22 +3,22 @@ package com.therealbluepandabear.pixapencil.activities.canvas
 import android.os.Handler
 import android.os.Looper
 import com.therealbluepandabear.pixapencil.converters.BitmapConverter
-import com.therealbluepandabear.pixapencil.utility.LongConstants
 
 fun CanvasActivity.extendedOnResume() {
-    if (prevBitmapStr != "") {
-        Handler(Looper.getMainLooper()).postDelayed({
-            val convertedBMP = BitmapConverter.convertStringToBitmap(prevBitmapStr)
-            if (convertedBMP != null) {
-                outerCanvasInstance.canvasFragment.myCanvasViewInstance.replaceBitmap(convertedBMP)
-            }
-        }, LongConstants.DefaultHandlerDelay)
-    }
-
     if (prevOrientation != 0) {
         if (prevOrientation != resources.configuration.orientation) {
             onOrientationChanged()
             prevOrientation = resources.configuration.orientation
         }
     }
+
+    if (prevBitmapStr != "") {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val convertedBMP = BitmapConverter.convertStringToBitmap(prevBitmapStr)
+            if (convertedBMP != null) {
+                outerCanvasInstance.canvasFragment.myCanvasViewInstance.replaceBitmap(convertedBMP)
+            }
+        }, 1000)
+    }
+
 }
