9c3a1aa37b23e96b9e73a9f97430b2d9175051b9
thebluepandabear <justaguyoney@gmail.com>
2022-04-18 03:25:32 +1200

    Fixed major bug.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+onSaveInstanceState.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+onSaveInstanceState.kt
@@ -8,13 +8,36 @@ import com.therealbluepandabear.pixapencil.fragments.canvas.pixelGridViewInstanc
 import com.therealbluepandabear.pixapencil.utility.StringConstants
 
 fun CanvasActivity.extendedOnSaveInstanceState(outState: Bundle) {
-    outState.putInt(StringConstants.prevOrientationBundleIdentifier, resources.configuration.orientation)
-    outState.putString(StringConstants.prevBitmapStrBundleIdentifier, BitmapConverter.convertBitmapToString(pixelGridViewInstance.pixelGridViewBitmap))
-    outState.putInt(StringConstants.prevPrimaryColorBundleIdentifier, (binding.activityCanvasColorPrimaryView.background as ColorDrawable).color)
-    outState.putInt(StringConstants.prevSecondaryColorBundleIdentifier, (binding.activityCanvasColorSecondaryView.background as ColorDrawable).color)
-    outState.putString(StringConstants.prevToolBundleIdentifier, currentTool.toolName)
-    outState.putString(StringConstants.prevBrushBundleIdentifier, pixelGridViewInstance.currentBrush?.brushName)
-    outState.putInt(StringConstants.prevTabBundleIdentifier, currentTab)
-    outState.putString(StringConstants.prevUndoStackJsonStrIdentifier, JsonConverter.convertListToJsonString(pixelGridViewInstance.bitmapActionData))
-    outState.putString(StringConstants.prevRedoStackJsonStrIdentifier, JsonConverter.convertListToJsonString(pixelGridViewInstance.undoStack))
+    if (resources.configuration.orientation != 0) {
+        outState.putInt(
+            StringConstants.prevOrientationBundleIdentifier,
+            resources.configuration.orientation
+        )
+        outState.putString(
+            StringConstants.prevBitmapStrBundleIdentifier,
+            BitmapConverter.convertBitmapToString(pixelGridViewInstance.pixelGridViewBitmap)
+        )
+        outState.putInt(
+            StringConstants.prevPrimaryColorBundleIdentifier,
+            (binding.activityCanvasColorPrimaryView.background as ColorDrawable).color
+        )
+        outState.putInt(
+            StringConstants.prevSecondaryColorBundleIdentifier,
+            (binding.activityCanvasColorSecondaryView.background as ColorDrawable).color
+        )
+        outState.putString(StringConstants.prevToolBundleIdentifier, currentTool.toolName)
+        outState.putString(
+            StringConstants.prevBrushBundleIdentifier,
+            pixelGridViewInstance.currentBrush?.brushName
+        )
+        outState.putInt(StringConstants.prevTabBundleIdentifier, currentTab)
+        outState.putString(
+            StringConstants.prevUndoStackJsonStrIdentifier,
+            JsonConverter.convertListToJsonString(pixelGridViewInstance.bitmapActionData)
+        )
+        outState.putString(
+            StringConstants.prevRedoStackJsonStrIdentifier,
+            JsonConverter.convertListToJsonString(pixelGridViewInstance.undoStack)
+        )
+    }
 }
