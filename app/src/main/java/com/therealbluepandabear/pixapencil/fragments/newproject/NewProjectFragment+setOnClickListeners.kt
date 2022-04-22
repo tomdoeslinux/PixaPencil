12ad541a61d1e5e2669ea165c2fb21540be1d9df
thebluepandabear <justaguyoney@gmail.com>
2022-04-22 14:15:51 +1200

    Improved checking for valid values in the 'New Project' fragment by adding errors underneath the EditTexts if the values are invalid, etc.

--- app/src/main/java/com/therealbluepandabear/pixapencil/fragments/newproject/NewProjectFragment+setOnClickListeners.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/fragments/newproject/NewProjectFragment+setOnClickListeners.kt
@@ -1,55 +1,81 @@
 package com.therealbluepandabear.pixapencil.fragments.newproject
 
+import androidx.core.widget.doAfterTextChanged
 import com.therealbluepandabear.pixapencil.R
-import com.therealbluepandabear.pixapencil.enums.SnackbarDuration
-import com.therealbluepandabear.pixapencil.extensions.showSnackbar
 import com.therealbluepandabear.pixapencil.utility.HapticFeedbackWrapper
 import com.therealbluepandabear.pixapencil.utility.IntConstants
 
+var invalidValues = false
+
+private fun NewProjectFragment.checkForTitleError() {
+    val title = binding.fragmentNewCanvasProjectTitleTextInputEditText.text.toString()
+
+    if (title.isBlank()) {
+        binding.fragmentNewCanvasProjectTitleTextInputLayout.error = getString(R.string.exception_invalid_title_in_code_str)
+        invalidValues = true
+    } else {
+        binding.fragmentNewCanvasProjectTitleTextInputLayout.error = null
+        invalidValues = false
+    }
+}
+
+private fun NewProjectFragment.checkForWidthError() {
+    val widthValue: Int = binding.fragmentNewCanvasWidthTextInputEditText.text.toString().toIntOrNull() ?: 0
+
+    if (widthValue !in IntConstants.SpanCountMin..IntConstants.SpanCountMax) {
+        binding.fragmentNewCanvasWidthTextInputLayout.error = getString(R.string.exception_invalid_width_in_code_str)
+        invalidValues = true
+    } else {
+        binding.fragmentNewCanvasWidthTextInputLayout.error = null
+        invalidValues = false
+    }
+}
+
+private fun NewProjectFragment.checkForHeightError() {
+    val heightValue: Int = binding.fragmentNewCanvasHeightTextInputEditText.text.toString().toIntOrNull() ?: 0
+
+    if (heightValue !in IntConstants.SpanCountMin..IntConstants.SpanCountMax) {
+        binding.fragmentNewCanvasHeightTextInputLayout.error = getString(R.string.exception_invalid_height_in_code_str)
+        invalidValues = true
+    } else {
+        binding.fragmentNewCanvasHeightTextInputLayout.error = null
+        invalidValues = false
+    }
+}
+
 fun NewProjectFragment.setOnClickListeners() {
     binding.apply {
+        fragmentNewCanvasProjectTitleTextInputEditText.doAfterTextChanged {
+            checkForTitleError()
+        }
+
+        fragmentNewCanvasWidthTextInputEditText.doAfterTextChanged {
+            checkForWidthError()
+        }
+
+        fragmentNewCanvasHeightTextInputEditText.doAfterTextChanged {
+            checkForHeightError()
+        }
+
         fragmentNewCanvasDoneButton.setOnClickListener {
-            val title = fragmentNewCanvasProjectTitleTextInputEditText.text.toString()
+            checkForTitleError()
+            checkForWidthError()
+            checkForHeightError()
 
-            val widthValue: Int = binding.fragmentNewCanvasWidthTextInputEditText.text.toString().toIntOrNull() ?: 0
-            val heightValue: Int = binding.fragmentNewCanvasHeightTextInputEditText.text.toString().toIntOrNull() ?: 0
-
-
-            if (widthValue !in IntConstants.SpanCountMin..IntConstants.SpanCountMax
-                ||
-                heightValue !in IntConstants.SpanCountMin..IntConstants.SpanCountMax
-            ) {
-                HapticFeedbackWrapper.performHapticFeedback(binding.fragmentNewCanvasDoneButton)
-                root.showSnackbar(
-                    getString(R.string.exception_invalid_width_height_message_in_code_str),
-                    SnackbarDuration.Default
-                )
-            } else if (title.isBlank()) {
-                root.showSnackbar(
-                    getString(R.string.exception_invalid_title_str),
-                    SnackbarDuration.Default
-                )
-            } else if (
-                binding.fragmentNewCanvasWidthTextInputEditText.text.toString().isBlank()
-                ||
-                binding.fragmentNewCanvasHeightTextInputEditText.text.toString().isBlank()) {
-                root.showSnackbar(
-                    getString(R.string.exception_invalid_title_str),
-                    SnackbarDuration.Default
-                )
-            } else {
+            if (!invalidValues) {
                 try {
+                    val title = fragmentNewCanvasProjectTitleTextInputEditText.text.toString()
+
+                    val widthValue: Int = binding.fragmentNewCanvasWidthTextInputEditText.text.toString().toInt()
+                    val heightValue: Int = binding.fragmentNewCanvasHeightTextInputEditText.text.toString().toInt()
+
                     caller.onDoneButtonPressed(
                         title,
                         widthValue,
                         heightValue
                     )
-                } catch (ex: Exception) {
+                } catch (exception: Exception) {
                     HapticFeedbackWrapper.performHapticFeedback(binding.fragmentNewCanvasDoneButton)
-                    root.showSnackbar(
-                        getString(R.string.exception_invalid_width_height_message_in_code_str),
-                        SnackbarDuration.Default
-                    )
                 }
             }
         }
