8b3f2f69514fdaaf5f4beea7d51bae7d9c167151
thebluepandabear <justaguyoney@gmail.com>
2022-04-25 16:29:10 +1200

    Replaced some more hardcoded strings to string resources in `strings.xml` (2). Renamed some variables.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+onCreationLongTapped.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+onCreationLongTapped.kt
@@ -6,24 +6,24 @@ import com.therealbluepandabear.pixapencil.database.AppData
 import com.therealbluepandabear.pixapencil.extensions.showDialog
 import com.therealbluepandabear.pixapencil.models.PixelArt
 
-fun MainActivity.extendedOnCreationLongTapped(param: PixelArt) {
+fun MainActivity.extendedOnCreationLongTapped(pixelArtObj: PixelArt) {
     (binding.activityMainRecentCreationsRecyclerView.adapter as PixelArtCreationsAdapter).userHasLongPressed =
         true
 
-    val paramTitle = param.title
+    val title = pixelArtObj.title
 
     if ((binding.activityMainRecentCreationsRecyclerView.adapter as PixelArtCreationsAdapter).userHasLongPressed) {
         showDialog(
-            "Delete '$paramTitle'?",
-            "Are you sure you want to delete '$paramTitle'? - this cannot be undone.",
+            getString(R.string.dialog_delete_pixel_art_project_title_in_code_str, title),
+            getString(R.string.dialog_delete_pixel_art_project_text_in_code_str, title),
             getString(R.string.dialog_positive_button_text_in_code_str), { _, _ ->
                 AppData.pixelArtDB.pixelArtCreationsDao().getAllPixelArtCreations().observe(this) {
 
                     AppData.pixelArtDB.pixelArtCreationsDao()
-                        .deletePixelArtCreation(param.objId)
+                        .deletePixelArtCreation(pixelArtObj.objId)
                     binding.activityMainRecentCreationsRecyclerView.adapter!!.notifyItemRemoved(
                         it.indexOf(
-                            param
+                            pixelArtObj
                         )
                     )
                 }
