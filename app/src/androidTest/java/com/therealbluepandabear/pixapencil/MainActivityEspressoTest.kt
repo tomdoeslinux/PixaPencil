5477a1515295555e015f9ba168cda9af4f4e50d5
thebluepandabear <justaguyoney@gmail.com>
2022-04-29 16:23:36 +1200

    Added Espresso test to validate that MainActivity's title is 'PixaPencil'.

--- app/src/androidTest/java/com/therealbluepandabear/pixapencil/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/therealbluepandabear/pixapencil/MainActivityEspressoTest.kt
@@ -1,16 +1,21 @@
 package com.therealbluepandabear.pixapencil
 
+import android.widget.TextView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.therealbluepandabear.pixapencil.activities.main.MainActivity
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.instanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -151,5 +156,13 @@ class MainActivityEspressoTest {
             onView(withId(R.id.activityMainTopAppMenu_community_item)).perform(click())
             pressBack()
         }
+    }
+
+    @Test
+    fun checkTitleIsPixaPencil() {
+        onView(allOf(instanceOf(
+            TextView::class.java),
+            withParent(withResourceName("action_bar")))
+        ).check(matches(withText("PixaPencil")))
     }
 }
