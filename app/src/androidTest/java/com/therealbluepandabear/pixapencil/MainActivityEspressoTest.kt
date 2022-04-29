2ec639a524e1629b363a201a9befa4f7f69b8732
thebluepandabear <justaguyoney@gmail.com>
2022-04-29 17:41:24 +1200

    Added some more Espresso tests (5).

--- app/src/androidTest/java/com/therealbluepandabear/pixapencil/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/therealbluepandabear/pixapencil/MainActivityEspressoTest.kt
@@ -442,4 +442,68 @@ class MainActivityEspressoTest {
         onView(withId(R.id.fragmentNewCanvas_heightTextInputEditText)).perform(typeText("1000"))
         onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
     }
+
+    @Test
+    fun create10x5() {
+        onView(withId(R.id.activityMain_newProjectButton)).perform(click())
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).perform(typeText(defaultProjectName))
+        onView(withId(R.id.fragmentNewCanvas_widthTextInputEditText)).perform(typeText("10"))
+        onView(withId(R.id.fragmentNewCanvas_heightTextInputEditText)).perform(typeText("5"))
+        onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
+    }
+
+    @Test
+    fun create50x10() {
+        onView(withId(R.id.activityMain_newProjectButton)).perform(click())
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).perform(typeText(defaultProjectName))
+        onView(withId(R.id.fragmentNewCanvas_widthTextInputEditText)).perform(typeText("50"))
+        onView(withId(R.id.fragmentNewCanvas_heightTextInputEditText)).perform(typeText("10"))
+        onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
+    }
+
+    @Test
+    fun create100x50() {
+        onView(withId(R.id.activityMain_newProjectButton)).perform(click())
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).perform(typeText(defaultProjectName))
+        onView(withId(R.id.fragmentNewCanvas_widthTextInputEditText)).perform(typeText("100"))
+        onView(withId(R.id.fragmentNewCanvas_heightTextInputEditText)).perform(typeText("50"))
+        onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
+    }
+
+    @Test
+    fun create200x30() {
+        onView(withId(R.id.activityMain_newProjectButton)).perform(click())
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).perform(typeText(defaultProjectName))
+        onView(withId(R.id.fragmentNewCanvas_widthTextInputEditText)).perform(typeText("200"))
+        onView(withId(R.id.fragmentNewCanvas_heightTextInputEditText)).perform(typeText("30"))
+        onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
+    }
+
+    @Test
+    fun create550x250() {
+        onView(withId(R.id.activityMain_newProjectButton)).perform(click())
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).perform(typeText(defaultProjectName))
+        onView(withId(R.id.fragmentNewCanvas_widthTextInputEditText)).perform(typeText("550"))
+        onView(withId(R.id.fragmentNewCanvas_heightTextInputEditText)).perform(typeText("250"))
+        onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
+    }
+
+    @Test
+    fun create1500x500() {
+        onView(withId(R.id.activityMain_newProjectButton)).perform(click())
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).perform(typeText(defaultProjectName))
+        onView(withId(R.id.fragmentNewCanvas_widthTextInputEditText)).perform(typeText("1500"))
+        onView(withId(R.id.fragmentNewCanvas_heightTextInputEditText)).perform(typeText("500"))
+        onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
+    }
+
+    @Test
+    fun create1000x100() {
+        onView(withId(R.id.activityMain_newProjectButton)).perform(click())
+        onView(withId(R.id.fragmentNewCanvas_projectTitleTextInputEditText)).perform(typeText(defaultProjectName))
+        onView(withId(R.id.fragmentNewCanvas_widthTextInputEditText)).perform(typeText("1000"))
+        onView(withId(R.id.fragmentNewCanvas_heightTextInputEditText)).perform(typeText("100"))
+        onView(withId(R.id.fragmentNewCanvas_doneButton)).perform(click())
+    }
+
 }
