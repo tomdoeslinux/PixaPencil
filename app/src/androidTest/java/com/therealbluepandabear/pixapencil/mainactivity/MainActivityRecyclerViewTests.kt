005bd27102a8f16092e40b78c9eb320204b8abf6
thebluepandabear <justaguyoney@gmail.com>
2022-04-30 14:43:34 +1200

    Added some more Espresso tests.

--- app/src/androidTest/java/com/therealbluepandabear/pixapencil/mainactivity/MainActivityRecyclerViewTests.kt
+++ app/src/androidTest/java/com/therealbluepandabear/pixapencil/mainactivity/MainActivityRecyclerViewTests.kt
@@ -1,14 +1,17 @@
 package com.therealbluepandabear.pixapencil.mainactivity
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.therealbluepandabear.pixapencil.R
 import com.therealbluepandabear.pixapencil.activities.main.MainActivity
+import com.therealbluepandabear.pixapencil.activities.main.binding
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,11 +36,28 @@ class MainActivityRecyclerViewTests {
         onView(withId(R.id.activityMainTopAppMenu_community_item)).perform(click())
     }
 
+    private fun getRecyclerViewItemCount(): Int {
+        var itemCount = 0
+
+        activityRule.scenario.onActivity {
+            itemCount = binding.activityMainRecentCreationsRecyclerView.adapter!!.itemCount
+        }
+
+        return itemCount
+    }
+
+    private fun scrollBackAndForth() {
+        onView(withId(R.id.activityMain_recentCreationsRecyclerView)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(getRecyclerViewItemCount() - 1))
+        onView(withId(R.id.activityMain_recentCreationsRecyclerView)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0))
+    }
+
+    // Last completed successfully on April 29 at 9:24 PM
     @Test
     fun canHandle1Project() {
         createDummyProject()
     }
-
+    
+    // Last completed successfully on April 29 at 9:24 PM
     @Test
     fun canHandle25Projects() {
         for (i in 0..25) {
@@ -87,5 +107,153 @@ class MainActivityRecyclerViewTests {
         }
     }
 
+    @Test
+    fun canHandleScrollingBackAndForth50TimesWith1Project() {
+        for (i in 0..50) {
+            scrollBackAndForth()
+        }
+    }
 
+    @Test
+    fun canHandleScrollingBackAndForth50TimesWith25Projects() {
+        for (i in 0..25) {
+            createDummyProject()
+        }
+
+        for (i in 0..50) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth50TimesWith50Projects() {
+        for (i in 0..50) {
+            createDummyProject()
+        }
+
+        for (i in 0..50) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth50TimesWith100Projects() {
+        for (i in 0..100) {
+            createDummyProject()
+        }
+
+        for (i in 0..50) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth50TimesWith250Projects() {
+        for (i in 0..250) {
+            createDummyProject()
+        }
+
+        for (i in 0..50) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth50TimesWith500Projects() {
+        for (i in 0..500) {
+            createDummyProject()
+        }
+
+        for (i in 0..50) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth100TimesWith1000Projects() {
+        for (i in 0..1000) {
+            createDummyProject()
+        }
+
+        for (i in 0..50) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth200TimesWith2000Projects() {
+        for (i in 0..2000) {
+            createDummyProject()
+        }
+
+        for (i in 0..50) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth150TimesWith50Projects() {
+        for (i in 0..50) {
+            createDummyProject()
+        }
+
+        for (i in 0..150) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth150TimesWith100Projects() {
+        for (i in 0..100) {
+            createDummyProject()
+        }
+
+        for (i in 0..150) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth150TimesWith250Projects() {
+        for (i in 0..250) {
+            createDummyProject()
+        }
+
+        for (i in 0..150) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth150TimesWith500Projects() {
+        for (i in 0..500) {
+            createDummyProject()
+        }
+
+        for (i in 0..150) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth150TimesWith1000Projects() {
+        for (i in 0..1000) {
+            createDummyProject()
+        }
+
+        for (i in 0..150) {
+            scrollBackAndForth()
+        }
+    }
+
+    @Test
+    fun canHandleScrollingBackAndForth150TimesWith2000Projects() {
+        for (i in 0..2000) {
+            createDummyProject()
+        }
+
+        for (i in 0..150) {
+            scrollBackAndForth()
+        }
+    }
 }
