04b8d706f64a3f1373a6188e572db7a3afc3a553
thebluepandabear <justaguyoney@gmail.com>
2022-05-01 14:08:43 +1200

    Added `PixelArtCreationsDaoTest.kt` (Room DB test).

---
+++ app/src/androidTest/java/com/therealbluepandabear/pixapencil/data/local/PixelArtCreationsDaoTest.kt
@@ -0,0 +1,61 @@
+package com.therealbluepandabear.pixapencil.data.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.therealbluepandabear.pixapencil.dao.PixelArtCreationsDao
+import com.therealbluepandabear.pixapencil.database.PixelArtDatabase
+import com.therealbluepandabear.pixapencil.getOrAwaitValue
+import com.therealbluepandabear.pixapencil.models.PixelArt
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+
+class PixelArtCreationsDaoTest {
+    private lateinit var database: PixelArtDatabase
+    private lateinit var dao: PixelArtCreationsDao
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            PixelArtDatabase::class.java
+        ).allowMainThreadQueries().build()
+        dao = database.pixelArtCreationsDao()
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+    }
+
+    @Test
+    fun insertPixelArtCreation() {
+        runTest {
+            val pixelArtCreation = mockk<PixelArt>(relaxed = true)
+
+            every { pixelArtCreation.objId } returns 1
+
+            dao.insertPixelArt(pixelArtCreation)
+
+            val allPixelArtCreations = dao.getAllPixelArtCreations().getOrAwaitValue()
+
+            assert(allPixelArtCreations[0].objId == pixelArtCreation.objId)
+        }
+    }
+}
