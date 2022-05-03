552372f0a929d82fbf49939ffc5e6ca045e8d320
therealbluepandabear <justaguyoney@gmail.com>
2022-05-03 20:21:17 +1200

    Fixed major permission-based bug on Android Marshmallow.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+requestPermissions.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+requestPermissions.kt
@@ -1,10 +1,31 @@
 package com.therealbluepandabear.pixapencil.activities.main
 
 import android.Manifest
+import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
+/** Thank you to to Muhammad on StackOverflow - the code here is based off of their solution. It may be modified a bit.
+ *
+ * - [Link to Muhammad's profile](https://stackoverflow.com/users/1966247/muhammad)
+ * - [Original StackOverFlow post](https://stackoverflow.com/questions/33666071/android-marshmallow-request-permission?answertab=modifieddesc#tab-top)
+ * **/
+
+private fun MainActivity.askForPermissionOnSDK23(permission: String, requestCode: Int) {
+    if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+        }
+    }
+}
 fun MainActivity.requestPermissions() {
     val requestCode = 1
 
-    ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), requestCode)
+    if (android.os.Build.VERSION.SDK_INT != android.os.Build.VERSION_CODES.M) {
+        ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), requestCode)
+    } else {
+        askForPermissionOnSDK23(Manifest.permission.WRITE_EXTERNAL_STORAGE, requestCode)
+    }
 }
