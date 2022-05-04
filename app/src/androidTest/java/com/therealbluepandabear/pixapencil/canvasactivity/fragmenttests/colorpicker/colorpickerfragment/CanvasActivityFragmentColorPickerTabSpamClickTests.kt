9baa19b65fcbcf4e0e44688f9c4f65189e0340d3
thebluepandabear <justaguyoney@gmail.com>
2022-05-04 17:11:53 +1200

    Added some Espresso tests.

---
+++ app/src/androidTest/java/com/therealbluepandabear/pixapencil/canvasactivity/fragmenttests/colorpicker/colorpickerfragment/CanvasActivityFragmentColorPickerTabSpamClickTests.kt
@@ -0,0 +1,58 @@
+package com.therealbluepandabear.pixapencil.canvasactivity.fragmenttests.colorpicker.colorpickerfragment
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.longClick
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.therealbluepandabear.pixapencil.R
+import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class CanvasActivityFragmentColorPickerTabSpamClickTests {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(CanvasActivity::class.java)
+
+    private fun pressRGBTab() {
+        onView(withText(R.string.fragmentColorPicker_tab_rgb_str)).perform(longClick())
+    }
+
+    private fun pressHexTab() {
+        onView(withText(R.string.fragmentColorPicker_tab_hex_str)).perform(longClick())
+    }
+
+    private fun pressPickerTab() {
+        onView(withText(R.string.fragmentColorPicker_tab_color_picker_str)).perform(longClick())
+    }
+
+    @Test
+    fun click_PickerTab() {
+        pressPickerTab()
+    }
+
+    @Test
+    fun click_PickerTab_25Times() {
+        for (i in 0..25) {
+            pressPickerTab()
+        }
+    }
+
+    @Test
+    fun click_PickerTab_50Times() {
+        for (i in 0..50) {
+            pressPickerTab()
+        }
+    }
+
+    @Test
+    fun click_PickerTab_100Times() {
+        for (i in 0..100) {
+            pressPickerTab()
+        }
+    }
+}
--- app/src/androidTest/java/com/therealbluepandabear/pixapencil/canvasactivity/fragmenttests/colorpicker/colorpickerfragment/CanvasActivityFragmentColorPickerTabViewDisplayedTests.kt
+++ app/src/androidTest/java/com/therealbluepandabear/pixapencil/canvasactivity/fragmenttests/colorpicker/colorpickerfragment/CanvasActivityFragmentColorPickerTabViewDisplayedTests.kt
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class CanvasActivityFragmentColorPickerTabTests {
+class CanvasActivityFragmentColorPickerTabViewDisplayedTests {
     @get:Rule
     val activityRule = ActivityScenarioRule(CanvasActivity::class.java)
 
