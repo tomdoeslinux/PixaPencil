40b56f4a595da9c28aea54b1e3348664f3b97d08
therealbluepandabear <justaguyoney@gmail.com>
2022-05-05 08:05:52 +1200

    Fixed major orientation bug (ignore previous commit) (2).

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+onResume.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+onResume.kt
@@ -1,7 +1,7 @@
 package com.therealbluepandabear.pixapencil.activities.canvas
 
-import android.os.Handler
-import android.os.Looper
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.therealbluepandabear.pixapencil.R
 import com.therealbluepandabear.pixapencil.converters.BitmapConverter
 import com.therealbluepandabear.pixapencil.converters.JsonConverter
@@ -10,108 +10,9 @@ import com.therealbluepandabear.pixapencil.enums.SymmetryMode
 import com.therealbluepandabear.pixapencil.enums.Tools
 import com.therealbluepandabear.pixapencil.extensions.enable
 import com.therealbluepandabear.pixapencil.fragments.canvas.pixelGridViewInstance
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 fun CanvasActivity.extendedOnResume() {
-    if (prevOrientation != 0) {
-        if (prevOrientation != resources.configuration.orientation) {
-            onOrientationChanged()
-            prevOrientation = resources.configuration.orientation
-        }
-    }
-
-    if (prevBitmapStr != null && prevOrientation != 0) {
-        Handler(Looper.getMainLooper()).postDelayed({
-            val convertedBMP = BitmapConverter.convertStringToBitmap(prevBitmapStr!!)
-            if (convertedBMP != null) {
-                pixelGridViewInstance.replaceBitmap(convertedBMP)
-            }
-        }, 1000)
-    }
-
-    if (prevPrimaryColor != null && prevSecondaryColor != null && prevOrientation != 0) {
-        Handler().postDelayed({
-            setPrimaryPixelColor(prevPrimaryColor!!)
-            setSecondaryPixelColor(prevSecondaryColor!!)
-        }, 1000)
-    }
-
-    if (prevToolStr != null && prevOrientation != 0) {
-        Handler().postDelayed({
-            currentTool = if (Tools.findToolByName(prevToolStr!!) != null) {
-                Tools.findToolByName(prevToolStr!!)!!
-            } else {
-                Tools.PencilTool
-            }
-
-            toolsFragmentInstance?.tapOnToolByName(prevToolStr!!)
-        }, 1000)
-    }
-
-    if (prevBrushStr != null && prevOrientation != 0) {
-        Handler().postDelayed({
-            pixelGridViewInstance.currentBrush = BrushesDatabase.toList().find { it.brushName == prevBrushStr }
-        }, 1000)
-    }
-
-    if (prevTab != 0 && prevOrientation != 0) {
-        Handler().postDelayed({
-            binding.activityCanvasTabLayout.getTabAt(prevTab)?.select()
-        }, 1000)
-    }
-
-    if (prevUndoToolbarButtonDisabledEnabledState && prevOrientation != 0) {
-        Handler().postDelayed({
-            menu.findItem(R.id.activityCanvasTopAppMenu_undo).enable()
-        }, 1000)
-    }
-
-    if (prevRedoToolbarButtonDisabledEnabledState && prevOrientation != 0) {
-        Handler().postDelayed({
-            menu.findItem(R.id.activityCanvasTopAppMenu_redo_item).enable()
-        }, 1000)
-    }
-
-    if (prevUndoStackJsonStr != null && prevOrientation != 0) {
-        Handler().postDelayed({
-            pixelGridViewInstance.bitmapActionData = JsonConverter.convertJsonStringToListOfBitmapAction(
-                prevUndoStackJsonStr!!
-            ).toMutableList()
-        }, 1000)
-    }
-
-    if (prevRedoStackJsonStr != null && prevOrientation != 0) {
-        Handler().postDelayed({
-            pixelGridViewInstance.undoStack = JsonConverter.convertJsonStringToListOfBitmapAction(
-                prevRedoStackJsonStr!!
-            ).toMutableList()
-        }, 1000)
-    }
-
-    if (prevSymmetryModeStr != null && prevOrientation != 0) {
-        Handler().postDelayed({
-            pixelGridViewInstance.symmetryMode = SymmetryMode.values().first { it.symmetryName == prevSymmetryModeStr }
-
-            when (pixelGridViewInstance.symmetryMode) {
-                SymmetryMode.Horizontal -> {
-                    menu.findItem(R.id.appMenu_symmetry_horizontal_subItem).isChecked = true
-                }
-
-                SymmetryMode.Vertical -> {
-                    menu.findItem(R.id.appMenu_symmetry_vertical_subItem).isChecked = true
-                }
-
-                SymmetryMode.Quad -> {
-                    menu.findItem(R.id.appMenu_symmetry_quad_subItem).isChecked = true
-                }
-
-                else -> { }
-            }
-        }, 1000)
-    }
-
-    if (prevRotation != 0 && prevOrientation != 0) {
-        Handler().postDelayed({
-           outerCanvasInstance.rotate(prevRotation, animate = false)
-        }, 1000)
-    }
 }
