89cd9fb591c8020cc9b4c01a3a2e73687e5ef737
therealbluepandabear <justaguyoney@gmail.com>
2022-05-05 08:06:42 +1200

    Removed unused import directives.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity.kt
@@ -1,10 +1,7 @@
 package com.therealbluepandabear.pixapencil.activities.canvas
 
-import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
