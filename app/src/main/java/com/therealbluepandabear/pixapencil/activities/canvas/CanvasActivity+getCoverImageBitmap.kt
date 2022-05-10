da643f0630e3ebff4e5c39043c4575bd49eaef49
therealbluepandabear <justaguyoney@gmail.com>
2022-05-10 21:48:48 +1200

    Major: fixed bug in which the app wouldn't display images properly which have different width/height values.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+getCoverImageBitmap.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+getCoverImageBitmap.kt
@@ -1,36 +1,58 @@
 package com.therealbluepandabear.pixapencil.activities.canvas
 
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.Matrix
 import androidx.core.view.drawToBitmap
 import com.therealbluepandabear.pixapencil.fragments.canvas.pixelGridViewInstance
 import com.therealbluepandabear.pixapencil.utility.BitmapUtilities
+import com.therealbluepandabear.pixapencil.utility.OverlayType
 
 var gridWasEnabled = false
 
-fun CanvasActivity.getCoverImageBitmap(): Bitmap {
+fun disableGridIfNeeded() {
     if (pixelGridViewInstance.gridEnabled) {
         pixelGridViewInstance.gridEnabled = false
         pixelGridViewInstance.invalidate()
         gridWasEnabled = true
     }
+}
 
-    val bmp = outerCanvasInstance.fragmentHost.drawToBitmap()
-    val bmp2 = outerCanvasInstance.transparentBackgroundView.drawToBitmap()
+fun enableGridIfNeeded() {
+    if (gridWasEnabled) {
+        pixelGridViewInstance.gridEnabled = false
+        pixelGridViewInstance.invalidate()
+    }
+}
 
-    val combinedBmp = BitmapUtilities.overlay(bmp2, bmp)
+fun CanvasActivity.createBasicCoverBitmap(): Bitmap {
+    val primaryBitmap = outerCanvasInstance.fragmentHost.drawToBitmap()
+    val transparentBackgroundBitmap = outerCanvasInstance.transparentBackgroundView.drawToBitmap()
+    return BitmapUtilities.overlay(transparentBackgroundBitmap, primaryBitmap)
+}
+
+fun CanvasActivity.getCoverImageBitmap(): Bitmap {
+    disableGridIfNeeded()
+    var coverBitmap = createBasicCoverBitmap()
+
+    if (coverBitmap.width > coverBitmap.height) {
+        val bitmap = Bitmap.createBitmap(outerCanvasInstance.fragmentHost.width, outerCanvasInstance.fragmentHost.width, Bitmap.Config.ARGB_8888)
+        bitmap.eraseColor(Color.WHITE)
+        coverBitmap = BitmapUtilities.overlay(bitmap, coverBitmap, OverlayType.CenterSecond)
+    } else if (coverBitmap.height > coverBitmap.width) {
+        val bitmap = Bitmap.createBitmap(outerCanvasInstance.fragmentHost.height, outerCanvasInstance.fragmentHost.height, Bitmap.Config.ARGB_8888)
+        bitmap.eraseColor(Color.WHITE)
+        coverBitmap = BitmapUtilities.overlay(bitmap, coverBitmap, OverlayType.CenterSecond)
+    }
 
     val bmps: Bitmap?
 
     val matrix = Matrix()
     matrix.setRotate(outerCanvasInstance.getCurrentRotation())
 
-    bmps = Bitmap.createBitmap(combinedBmp, 0, 0, combinedBmp.width, combinedBmp.height, matrix, false)
+    bmps = Bitmap.createBitmap(coverBitmap, 0, 0, coverBitmap.width, coverBitmap.height, matrix, false)
 
-    if (gridWasEnabled) {
-        pixelGridViewInstance.gridEnabled = false
-        pixelGridViewInstance.invalidate()
-    }
+    enableGridIfNeeded()
 
     return bmps
 }
--- app/src/main/java/com/therealbluepandabear/pixapencil/utility/BitmapUtilities.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/utility/BitmapUtilities.kt
@@ -4,6 +4,11 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Matrix
 
+enum class OverlayType {
+    Regular,
+    CenterSecond
+}
+
 object BitmapUtilities {
     /** Thank you to to RajaReddy PolamReddy on StackOverflow - the code here is  based off of their solution.
      *
@@ -11,16 +16,25 @@ object BitmapUtilities {
      * - [Original StackOverFlow post](https://stackoverflow.com/questions/10616777/how-to-merge-to-two-bitmap-one-over-another)
      * **/
 
-    fun overlay(bmp1: Bitmap, bmp2: Bitmap): Bitmap {
-        val bimapOverlay = Bitmap.createBitmap(bmp1.width, bmp1.height, bmp1.config)
-        val canvas = Canvas(bimapOverlay)
+    fun overlay(bmp1: Bitmap, bmp2: Bitmap, overlayType: OverlayType = OverlayType.Regular): Bitmap {
+        val bitmapOverlay = Bitmap.createBitmap(bmp1.width, bmp1.height, bmp1.config)
+        val canvas = Canvas(bitmapOverlay)
 
-        canvas.drawBitmap(bmp1, Matrix(), null)
-        canvas.drawBitmap(bmp2, 0f, 0f, null)
+        if (overlayType == OverlayType.Regular) {
+            canvas.drawBitmap(bmp1, Matrix(), null)
+            canvas.drawBitmap(bmp2, 0f, 0f, null)
+        } else {
+            val startX = (canvas.width - bmp2.width) / 2
+            val startY = (canvas.height - bmp2.height) / 2
+
+            canvas.drawBitmap(bmp1, Matrix(), null)
+            canvas.drawBitmap(bmp2, startX.toFloat(), startY.toFloat(), null)
+        }
+
         bmp1.recycle()
         bmp2.recycle()
 
-        return bimapOverlay
+        return bitmapOverlay
     }
 
 
