f467bc468c0c91a86e683512b0368a6d34d9c9eb
therealbluepandabear <justaguyoney@gmail.com>
2022-05-15 14:20:16 +1200

    Fixed primary/secondary colour bug, solution was to disable `firstLoad` when `onPause()` is called.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+onPause.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity+onPause.kt
@@ -1,7 +1,9 @@
 package com.therealbluepandabear.pixapencil.activities.canvas
 
+import com.therealbluepandabear.pixapencil.activities.canvas.oncreate.firstLoad
 import com.therealbluepandabear.pixapencil.utility.Flags
 
 fun extendedOnPause() {
     Flags.DisableActionMove = false
+    firstLoad = false
 }
