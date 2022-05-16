edaa5811d70bae7a7be474ea294316439a105fd2
therealbluepandabear <justaguyoney@gmail.com>
2022-05-16 19:18:13 +1200

    Added transparent view indicator to the colour picker RecyclerView. If the user adds a transparent colour to their palette, they will see the appropriate indication, etc.

--- app/src/main/java/com/therealbluepandabear/pixapencil/adapters/ColorPickerAdapter.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/adapters/ColorPickerAdapter.kt
@@ -3,37 +3,41 @@ package com.therealbluepandabear.pixapencil.adapters
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.core.graphics.BlendModeColorFilterCompat
 import androidx.core.graphics.BlendModeCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.therealbluepandabear.pixapencil.R
 import com.therealbluepandabear.pixapencil.converters.JsonConverter
+import com.therealbluepandabear.pixapencil.databinding.ColorPickerLayoutBinding
 import com.therealbluepandabear.pixapencil.listeners.ColorPickerListener
 import com.therealbluepandabear.pixapencil.models.ColorPalette
-import com.therealbluepandabear.pixapencil.viewholders.ColorPickerViewHolder
+import com.therealbluepandabear.pixapencil.viewholders.ViewHolder
 
 class ColorPickerAdapter(
     private val data: ColorPalette,
     private val caller: ColorPickerListener?,
     private val isPaletteMode: Boolean = true,
-    private val isPreviewMode: Boolean = false) : RecyclerView.Adapter<ColorPickerViewHolder>() {
+    private val isPreviewMode: Boolean = false) : RecyclerView.Adapter<ViewHolder<FrameLayout>>() {
+    private lateinit var binding: ColorPickerLayoutBinding
+
     private var colorData = listOf<Int>()
 
     init {
         colorData = JsonConverter.convertJsonStringToListOfInt(data.colorPaletteColorData)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorPickerViewHolder {
-        return ColorPickerViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.color_picker_layout, parent, false) as View)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<FrameLayout> {
+        binding = ColorPickerLayoutBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(binding.colorPickerLayoutRootLayout)
     }
 
-    override fun onBindViewHolder(holder: ColorPickerViewHolder, position: Int) {
-        holder.colorView.backgroundTintList = ColorStateList.valueOf(colorData[position])
+    override fun onBindViewHolder(holder: ViewHolder<FrameLayout>, position: Int) {
+        binding.colorView.backgroundTintList = ColorStateList.valueOf(colorData[position])
 
         if (!isPreviewMode) {
-            holder.colorView.setOnLongClickListener {
+            binding.colorView.setOnLongClickListener {
                 if (colorData[position] != Color.TRANSPARENT) {
                     caller!!.onColorLongTapped(data, position)
                 }
@@ -43,12 +47,12 @@ class ColorPickerAdapter(
 
         if (isPaletteMode && !isPreviewMode) {
             if (colorData[position] == Color.TRANSPARENT && colorData[position] == colorData.last()) {
-                holder.colorView.setBackgroundResource(R.drawable.ic_baseline_add_24)
-                holder.colorView.background.colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(Color.GRAY, BlendModeCompat.DST_OVER)
+                binding.colorView.setBackgroundResource(R.drawable.ic_baseline_add_24)
+                binding.colorView.background.colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(Color.GRAY, BlendModeCompat.DST_OVER)
             }
         }
 
-        holder.colorView.setOnClickListener {
+        binding.colorView.setOnClickListener {
             if (colorData[position] != Color.TRANSPARENT) {
                 caller?.onColorTapped(colorData[position], it)
             } else if (colorData[position] == Color.TRANSPARENT){
@@ -57,7 +61,7 @@ class ColorPickerAdapter(
         }
 
         if (position == 0) {
-            caller?.onColorTapped(colorData[position], holder.colorView)
+            caller?.onColorTapped(colorData[position], binding.colorView)
         }
     }
 
--- app/src/main/java/com/therealbluepandabear/pixapencil/viewholders/ColorPickerViewHolder.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/viewholders/ColorPickerViewHolder.kt
@@ -3,4 +3,4 @@ package com.therealbluepandabear.pixapencil.viewholders
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class ColorPickerViewHolder(val colorView: View) : RecyclerView.ViewHolder(colorView)
+class ColorPickerViewHolder(val frameLayout: View) : RecyclerView.ViewHolder(frameLayout)
