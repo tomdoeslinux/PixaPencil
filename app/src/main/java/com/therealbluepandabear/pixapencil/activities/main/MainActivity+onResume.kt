0c25c6fc54c175b12e6c790f8129a6503579c437
Yousuf <yousuf.kuet08@gmail.com>
2022-05-20 00:21:30 +0600

    Remove code inside onResume as its not needed

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+onResume.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+onResume.kt
@@ -1,18 +1,4 @@
 package com.therealbluepandabear.pixapencil.activities.main
 
-import android.view.View
-import androidx.recyclerview.widget.GridLayoutManager
-import com.therealbluepandabear.pixapencil.adapters.PixelArtCreationsAdapter
-import com.therealbluepandabear.pixapencil.database.AppData
-
 fun MainActivity.extendedOnResume() {
-    binding.apply {
-        activityMainRecentCreationsRecyclerView.visibility = View.VISIBLE
-        activityMainNewProjectButton.show()
-        activityMainRecentCreationsRecyclerView.layoutManager = GridLayoutManager(this@extendedOnResume, 2)
-    }
-
-    AppData.pixelArtDB.pixelArtCreationsDao().getAllPixelArtCreations().observe(this) {
-        binding.activityMainRecentCreationsRecyclerView.adapter = PixelArtCreationsAdapter(binding.clayout, it, this)
-    }
 }
