73da9640e1e0668ff747028fa442f3bab5573a57
therealbluepandabear <justaguyoney@gmail.com>
2022-05-20 15:19:14 +1200

    Fixed bug where the undo/redo menu items would stay greyed out after the user would find and replace colors.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/CanvasActivity.kt
@@ -61,6 +61,7 @@ class CanvasActivity :
 
     var replacedBMP = false
 
+
     override fun onStart() {
         super.onStart()
         savePrevOrientationInfo()
--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/ondonebuttonpressed/CanvasActivity+FindAndReplaceFragment+onDoneButtonPressed.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/ondonebuttonpressed/CanvasActivity+FindAndReplaceFragment+onDoneButtonPressed.kt
@@ -1,12 +1,21 @@
 package com.therealbluepandabear.pixapencil.activities.canvas.ondonebuttonpressed
 
+import androidx.lifecycle.lifecycleScope
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.activities.canvas.canvascommands.replacePixelsByColor
+import com.therealbluepandabear.pixapencil.activities.canvas.judgeUndoRedoStacks
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 fun CanvasActivity.extendedOnDoneButtonPressed(colorToFind: Int?, colorToReplace: Int?) {
     supportFragmentManager.popBackStackImmediate()
 
     if (colorToFind != null && colorToReplace != null) {
         canvasCommandsHelperInstance.replacePixelsByColor(colorToFind, colorToReplace)
+
+        lifecycleScope.launch {
+            delay(50)
+            judgeUndoRedoStacks()
+        }
     }
 }
