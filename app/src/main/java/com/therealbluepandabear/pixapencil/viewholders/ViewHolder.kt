681c857636c074306f88637f2a749d58a8e86c54
therealbluepandabear <justaguyoney@gmail.com>
2022-05-25 15:18:13 +1200

    Removed unused `ViewHolder<T>` class.

--- app/src/main/java/com/therealbluepandabear/pixapencil/viewholders/ViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.therealbluepandabear.pixapencil.viewholders
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class ViewHolder<T : View>(viewHolder: T) : RecyclerView.ViewHolder(viewHolder)
