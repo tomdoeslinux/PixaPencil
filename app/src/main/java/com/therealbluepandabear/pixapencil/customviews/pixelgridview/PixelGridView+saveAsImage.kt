fa5f47cee591f24877ab57dd8c24e96be1970148
therealbluepandabear <justaguyoney@gmail.com>
2022-05-26 17:11:31 +1200

    Added swipe-to-dismiss behaviour for the rest of in-app snackbars.

--- app/src/main/java/com/therealbluepandabear/pixapencil/customviews/pixelgridview/PixelGridView+saveAsImage.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/customviews/pixelgridview/PixelGridView+saveAsImage.kt
@@ -5,6 +5,8 @@ import android.graphics.Bitmap
 import android.net.Uri
 import androidx.core.view.drawToBitmap
 import com.therealbluepandabear.pixapencil.R
+import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
+import com.therealbluepandabear.pixapencil.activities.canvas.binding
 import com.therealbluepandabear.pixapencil.enums.OutputCode
 import com.therealbluepandabear.pixapencil.enums.SnackbarDuration
 import com.therealbluepandabear.pixapencil.extensions.showSimpleInfoDialog
@@ -22,19 +24,27 @@ fun PixelGridView.extendedSaveAsImage(format: Bitmap.CompressFormat, thisRotatio
     val bitmap = outerCanvasInstance.fragmentHost.drawToBitmap()
     val fileHelperUtilitiesInstance = FileHelperUtilities.createInstance(context)
 
+    var cntxView = if (context is CanvasActivity) {
+        binding.clayout
+    } else this
+
+    if (cntxView == null) {
+        cntxView = this
+    }
+
     fileHelperUtilitiesInstance.saveBitmapAsImage(bitmap, projectTitle,90, format, rotation = thisRotation) { outputCode, _file, exceptionMessage_1 ->
         if (outputCode == OutputCode.Success) {
             file = _file
 
-            showSnackbarWithAction(context.getString(R.string.snackbar_image_successfully_saved_in_code_str, projectTitle, formatName), SnackbarDuration.Medium, context.getString(R.string.snackbar_view_exception_info_button_text_in_code_str)) {
+            cntxView.showSnackbarWithAction(context.getString(R.string.snackbar_image_successfully_saved_in_code_str, projectTitle, formatName), SnackbarDuration.Medium, context.getString(R.string.snackbar_view_exception_info_button_text_in_code_str)) {
                 fileHelperUtilitiesInstance.openImageFromUri(Uri.fromFile(file)) { outputCode, exceptionMessage_2 ->
                     if (outputCode == OutputCode.Failure) {
                         if (exceptionMessage_2 != null) {
-                            showSnackbarWithAction(context.getString(R.string.dialog_view_file_error_title_in_code_str), SnackbarDuration.Default, context.getString(R.string.dialog_exception_info_title_in_code_str)) {
+                            cntxView.showSnackbarWithAction(context.getString(R.string.dialog_view_file_error_title_in_code_str), SnackbarDuration.Default, context.getString(R.string.dialog_exception_info_title_in_code_str)) {
                                 (context as Activity).showSimpleInfoDialog(context.getString(R.string.dialog_exception_info_title_in_code_str), exceptionMessage_2)
                             }
                         } else {
-                            showSnackbar(context.getString(R.string.dialog_view_file_error_title_in_code_str), SnackbarDuration.Default)
+                            cntxView.showSnackbar(context.getString(R.string.dialog_view_file_error_title_in_code_str), SnackbarDuration.Default)
                         }
                     } else {
                         Flags.PressedBackFromImg = true
@@ -43,11 +53,11 @@ fun PixelGridView.extendedSaveAsImage(format: Bitmap.CompressFormat, thisRotatio
             }
         } else {
             if (exceptionMessage_1 != null) {
-                showSnackbarWithAction(context.getString(R.string.dialog_image_exception_when_saving_title_in_code_str, projectTitle, formatName), SnackbarDuration.Default, context.getString(R.string.dialog_exception_info_title_in_code_str)) {
+                cntxView.showSnackbarWithAction(context.getString(R.string.dialog_image_exception_when_saving_title_in_code_str, projectTitle, formatName), SnackbarDuration.Default, context.getString(R.string.dialog_exception_info_title_in_code_str)) {
                     (context as Activity).showSimpleInfoDialog(context.getString(R.string.dialog_exception_info_title_in_code_str), exceptionMessage_1)
                 }
             } else {
-                showSnackbar(context.getString(R.string.dialog_image_exception_when_saving_title_in_code_str, projectTitle, formatName), SnackbarDuration.Default)
+                cntxView.showSnackbar(context.getString(R.string.dialog_image_exception_when_saving_title_in_code_str, projectTitle, formatName), SnackbarDuration.Default)
             }
         }
     }
