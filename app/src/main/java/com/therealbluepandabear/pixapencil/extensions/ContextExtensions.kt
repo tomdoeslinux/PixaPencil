428e785a618ccf625999fe149ff32ee18a853320
therealbluepandabear <justaguyoney@gmail.com>
2022-05-29 17:41:15 +1200

    Added `Context.copyToClipboard` extension function.

--- app/src/main/java/com/therealbluepandabear/pixapencil/extensions/ContextExtensions.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/extensions/ContextExtensions.kt
@@ -1,8 +1,12 @@
 package com.therealbluepandabear.pixapencil.extensions
 
 import android.app.Activity
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.ContextWrapper
+import com.therealbluepandabear.pixapencil.enums.OutputCode
+import com.therealbluepandabear.pixapencil.fragments.colorpicker.hex.setOnClickListeners
 
 /** Thank you to to rjrjr on StackOverflow - the code here is  based off of their solution. It may be modified a bit.
  *
@@ -13,4 +17,15 @@ import android.content.ContextWrapper
 tailrec fun Context.activity(): Activity? = when (this) {
     is Activity -> this
     else -> (this as? ContextWrapper)?.baseContext?.activity()
+}
+
+fun Context.copyToClipboard(label: String, text: String): OutputCode {
+    val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    clipboard.setPrimaryClip(ClipData.newPlainText(label, text))
+
+    return if (clipboard.primaryClip?.getItemAt(0)?.text == text) {
+        OutputCode.Success
+    } else {
+        OutputCode.Failure
+    }
 }
--- app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/hex/HexadecimalColorPickerFragment+setOnClickListeners.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/hex/HexadecimalColorPickerFragment+setOnClickListeners.kt
@@ -4,17 +4,22 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.graphics.drawable.ColorDrawable
+import com.therealbluepandabear.pixapencil.enums.OutputCode
 import com.therealbluepandabear.pixapencil.enums.SnackbarDuration
+import com.therealbluepandabear.pixapencil.extensions.copyToClipboard
 import com.therealbluepandabear.pixapencil.extensions.showSnackbar
 import com.therealbluepandabear.pixapencil.fragments.colorpicker.caller
 import com.therealbluepandabear.pixapencil.fragments.colorpicker.colorPalette
 
 fun HexadecimalColorPickerFragment.setOnClickListeners() {
     binding.fragmentHexadecimalColorPickerHexadecimalValueTextInputLayout.setEndIconOnClickListener {
-        val clipboard = this@setOnClickListeners.requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        clipboard.setPrimaryClip(ClipData.newPlainText("hex_code", binding.fragmentHexadecimalColorPickerHexadecimalValueTextInputEditText.text))
+        val code = this@setOnClickListeners.requireContext().copyToClipboard("hex_code", binding.fragmentHexadecimalColorPickerHexadecimalValueTextInputEditText.text.toString())
 
-        binding.fragmentHexadecimalColorPickerCoordinatorLayout.showSnackbar("Successfully copied to clipboard", SnackbarDuration.Short)
+        if (code == OutputCode.Success) {
+            binding.fragmentHexadecimalColorPickerCoordinatorLayout.showSnackbar("Successfully copied to clipboard", SnackbarDuration.Short)
+        } else {
+            binding.fragmentHexadecimalColorPickerCoordinatorLayout.showSnackbar("Failed to copy text clipboard", SnackbarDuration.Short)
+        }
     }
 
     binding.fragmentHexadecimalColorPickerDoneButton.setOnClickListener {
