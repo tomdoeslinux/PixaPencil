67ded206035ac4222d3c71bbe13078c33b093411
therealbluepandabear <justaguyoney@gmail.com>
2022-05-29 13:21:18 +1200

    Bugfix (3).

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onpixeltapped/CanvasActivity+pencilTool+onPixelTapped.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onpixeltapped/CanvasActivity+pencilTool+onPixelTapped.kt
@@ -6,12 +6,14 @@ import com.therealbluepandabear.pixapencil.activities.canvas.getSelectedColor
 import com.therealbluepandabear.pixapencil.algorithms.LineAlgorithm
 import com.therealbluepandabear.pixapencil.algorithms.PixelPerfectAlgorithm
 import com.therealbluepandabear.pixapencil.database.BrushesDatabase
+import com.therealbluepandabear.pixapencil.enums.Tool
 import com.therealbluepandabear.pixapencil.extensions.getPixel
 import com.therealbluepandabear.pixapencil.fragments.canvas.pixelGridViewInstance
 import com.therealbluepandabear.pixapencil.models.Coordinates
 
 fun CanvasActivity.pencilToolOnPixelTapped(coordinatesTapped: Coordinates) {
-    val color = if (pixelGridViewInstance.pixelPerfectMode && (pixelGridViewInstance.currentBrush == BrushesDatabase.toList().first() || pixelGridViewInstance.currentBrush == null)) {
+    val isPxPerfect = (pixelGridViewInstance.pixelPerfectMode && currentTool == Tool.PencilTool && (pixelGridViewInstance.currentBrush == BrushesDatabase.toList().first() || pixelGridViewInstance.currentBrush == null))
+    val color = if (isPxPerfect) {
         pixelGridViewInstance.pixelGridViewBitmap.getPixel(coordinatesTapped)
     } else {
         getSelectedColor()
@@ -31,7 +33,7 @@ fun CanvasActivity.pencilToolOnPixelTapped(coordinatesTapped: Coordinates) {
     pixelGridViewInstance.prevY = coordinatesTapped.y
 
 
-    if (pixelGridViewInstance.pixelPerfectMode && (pixelGridViewInstance.currentBrush == BrushesDatabase.toList().first() || pixelGridViewInstance.currentBrush == null)) {
+    if (isPxPerfect) {
         for (i in PixelPerfectAlgorithm(primaryAlgorithmInfoParameter).compute()) {
             canvasCommandsHelperInstance.overrideSetPixel(i, getSelectedColor())
         }
