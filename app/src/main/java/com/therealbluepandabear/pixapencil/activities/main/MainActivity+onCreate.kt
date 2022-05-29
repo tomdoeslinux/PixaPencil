f812209a8ac9714db5cd4f85d6a1637cbdf9febc
therealbluepandabear <justaguyoney@gmail.com>
2022-05-29 17:33:03 +1200

    New feature: user can copy hex code to clipboard (fix #99).

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+onCreate.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+onCreate.kt
@@ -1,5 +1,9 @@
 package com.therealbluepandabear.pixapencil.activities.main
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+
 fun MainActivity.extendedOnCreate() {
     showWelcomeScreenIfApplicable()
     setVmPolicy()
--- app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/hex/HexadecimalColorPickerFragment+setOnClickListeners.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/hex/HexadecimalColorPickerFragment+setOnClickListeners.kt
@@ -1,10 +1,22 @@
 package com.therealbluepandabear.pixapencil.fragments.colorpicker.hex
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.graphics.drawable.ColorDrawable
+import com.therealbluepandabear.pixapencil.enums.SnackbarDuration
+import com.therealbluepandabear.pixapencil.extensions.showSnackbar
 import com.therealbluepandabear.pixapencil.fragments.colorpicker.caller
 import com.therealbluepandabear.pixapencil.fragments.colorpicker.colorPalette
 
 fun HexadecimalColorPickerFragment.setOnClickListeners() {
+    binding.fragmentHexadecimalColorPickerHexadecimalValueTextInputLayout.setEndIconOnClickListener {
+        val clipboard = this@setOnClickListeners.requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        clipboard.setPrimaryClip(ClipData.newPlainText("hex_code", binding.fragmentHexadecimalColorPickerHexadecimalValueTextInputEditText.text))
+
+        binding.fragmentHexadecimalColorPickerCoordinatorLayout.showSnackbar("Successfully copied to clipboard", SnackbarDuration.Short)
+    }
+
     binding.fragmentHexadecimalColorPickerDoneButton.setOnClickListener {
         try {
             caller.onDoneButtonPressed(
