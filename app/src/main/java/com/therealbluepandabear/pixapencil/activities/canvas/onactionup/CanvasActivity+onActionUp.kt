e6b93a6001c481ac738c7b992c2eedbdacf119bd
therealbluepandabear <justaguyoney@gmail.com>
2022-05-30 16:49:28 +1200

    Unfortunately reverted `a9327c3156f4d64183dedc33b218735a490ae9d5` (fix #100).

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onactionup/CanvasActivity+onActionUp.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onactionup/CanvasActivity+onActionUp.kt
@@ -1,11 +1,15 @@
 package com.therealbluepandabear.pixapencil.activities.canvas.onactionup
 
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
+import com.therealbluepandabear.pixapencil.activities.canvas.canvascommands.undo
 import com.therealbluepandabear.pixapencil.activities.canvas.getSelectedColor
 import com.therealbluepandabear.pixapencil.activities.canvas.judgeUndoRedoStacks
+import com.therealbluepandabear.pixapencil.database.BrushesDatabase
 import com.therealbluepandabear.pixapencil.enums.Tool
 import com.therealbluepandabear.pixapencil.enums.ToolFamily
 import com.therealbluepandabear.pixapencil.fragments.canvas.pixelGridViewInstance
+import com.therealbluepandabear.pixapencil.models.BitmapAction
+import com.therealbluepandabear.pixapencil.models.BitmapActionData
 
 fun resetPreviousCoordinates() {
     pixelGridViewInstance.prevX = null
@@ -45,8 +49,48 @@ fun CanvasActivity.extendedOnActionUp() {
         }
 
         else -> {
+            val isPxPerfect = (pixelGridViewInstance.pixelPerfectMode && currentTool == Tool.PencilTool && (pixelGridViewInstance.currentBrush == BrushesDatabase.toList().first() || pixelGridViewInstance.currentBrush == null))
+
             viewModel.bitmapActionData.add(viewModel.currentBitmapAction!!)
             resetPreviousCoordinates()
+
+            if (isPxPerfect) {
+                var distinct = viewModel.currentBitmapAction!!.actionData.distinctBy { it.coordinates }
+                val data = mutableListOf<BitmapActionData>()
+
+                var c = 0
+
+                while (c < distinct.size) {
+                    if (c > 0 && c + 1 < distinct.size
+                        && (distinct[c - 1].coordinates.x == distinct[c].coordinates.x || distinct[c - 1].coordinates.y == distinct[c].coordinates.y)
+                        && (distinct[c + 1].coordinates.x == distinct[c].coordinates.x || distinct[c + 1].coordinates.y == distinct[c].coordinates.y)
+                        && distinct[c - 1].coordinates.x != distinct[c + 1].coordinates.x
+                        && distinct[c - 1].coordinates.y != distinct[c + 1].coordinates.y
+                    ) {
+                        c += 1
+                    }
+
+                    data.add(distinct[c])
+
+                    c += 1
+                }
+
+                canvasCommandsHelperInstance.undo()
+
+                for (value in data) {
+                    distinct = distinct.filter { it == value }
+                }
+
+                for (value in data) {
+                    pixelGridViewInstance.pixelGridViewBitmap.setPixel(
+                        value.coordinates.x,
+                        value.coordinates.y,
+                        getSelectedColor()
+                    )
+                }
+
+                viewModel.bitmapActionData.add(BitmapAction(data))
+            }
         }
     }
 
--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onpixeltapped/CanvasActivity+pencilTool+onPixelTapped.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onpixeltapped/CanvasActivity+pencilTool+onPixelTapped.kt
@@ -4,38 +4,18 @@ import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.activities.canvas.canvascommands.overrideSetPixel
 import com.therealbluepandabear.pixapencil.activities.canvas.getSelectedColor
 import com.therealbluepandabear.pixapencil.algorithms.LineAlgorithm
-import com.therealbluepandabear.pixapencil.algorithms.PixelPerfectAlgorithm
-import com.therealbluepandabear.pixapencil.database.BrushesDatabase
-import com.therealbluepandabear.pixapencil.enums.Tool
-import com.therealbluepandabear.pixapencil.extensions.getPixel
 import com.therealbluepandabear.pixapencil.fragments.canvas.pixelGridViewInstance
 import com.therealbluepandabear.pixapencil.models.Coordinates
 
 fun CanvasActivity.pencilToolOnPixelTapped(coordinatesTapped: Coordinates) {
-    val isPxPerfect = (pixelGridViewInstance.pixelPerfectMode && currentTool == Tool.PencilTool && (pixelGridViewInstance.currentBrush == BrushesDatabase.toList().first() || pixelGridViewInstance.currentBrush == null))
-    val color = if (isPxPerfect) {
-        pixelGridViewInstance.pixelGridViewBitmap.getPixel(coordinatesTapped)
-    } else {
-        getSelectedColor()
-    }
-
     if (pixelGridViewInstance.prevX != null && pixelGridViewInstance.prevY != null) {
-        primaryAlgorithmInfoParameter.color = color
-
         val lineAlgorithmInstance = LineAlgorithm(primaryAlgorithmInfoParameter)
 
         lineAlgorithmInstance.compute(Coordinates(pixelGridViewInstance.prevX!!, pixelGridViewInstance.prevY!!), coordinatesTapped)
     }
 
-    canvasCommandsHelperInstance.overrideSetPixel(coordinatesTapped, color)
+    canvasCommandsHelperInstance.overrideSetPixel(coordinatesTapped, getSelectedColor())
 
     pixelGridViewInstance.prevX = coordinatesTapped.x
     pixelGridViewInstance.prevY = coordinatesTapped.y
-
-
-    if (isPxPerfect) {
-        for (i in PixelPerfectAlgorithm(primaryAlgorithmInfoParameter).compute()) {
-            canvasCommandsHelperInstance.overrideSetPixel(i, getSelectedColor())
-        }
-    }
 }
