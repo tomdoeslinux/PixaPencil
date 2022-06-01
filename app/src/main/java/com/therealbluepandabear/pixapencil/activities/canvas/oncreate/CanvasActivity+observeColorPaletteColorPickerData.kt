4c36f03397e5fb0c1f905ad8adc6efc2171f2ef6
therealbluepandabear <justaguyoney@gmail.com>
2022-06-01 16:29:25 +1200

    Partial fix #97 (2).

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/oncreate/CanvasActivity+observeColorPaletteColorPickerData.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/oncreate/CanvasActivity+observeColorPaletteColorPickerData.kt
@@ -1,14 +1,11 @@
 package com.therealbluepandabear.pixapencil.activities.canvas.oncreate
 
-import androidx.lifecycle.lifecycleScope
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.database.AppData
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 fun CanvasActivity.observeColorPaletteColorPickerData() {
     AppData.colorPalettesDB.colorPalettesDao().getAllColorPalettes().observe(this@observeColorPaletteColorPickerData) {
-        adapter.updateData(it.first())
+        adapter.updateData(it[selectedColorPaletteIndex])
     }
 }
  
