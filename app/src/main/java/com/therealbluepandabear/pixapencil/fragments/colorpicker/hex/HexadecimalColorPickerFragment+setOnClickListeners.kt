6648d77a8a1a7e4f87e32fe18a442e5b42cc19f9
therealbluepandabear <justaguyoney@gmail.com>
2022-06-02 15:17:08 +1200

    Clipboard fix.

--- app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/hex/HexadecimalColorPickerFragment+setOnClickListeners.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/hex/HexadecimalColorPickerFragment+setOnClickListeners.kt
@@ -1,6 +1,7 @@
 package com.therealbluepandabear.pixapencil.fragments.colorpicker.hex
 
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import com.therealbluepandabear.pixapencil.enums.OutputCode
 import com.therealbluepandabear.pixapencil.enums.SnackbarDuration
 import com.therealbluepandabear.pixapencil.extensions.copyToClipboard
@@ -12,10 +13,18 @@ fun HexadecimalColorPickerFragment.setOnClickListeners() {
     binding.fragmentHexadecimalColorPickerHexadecimalValueTextInputLayout.setEndIconOnClickListener {
         val code = this@setOnClickListeners.requireContext().copyToClipboard("hex_code", binding.fragmentHexadecimalColorPickerHexadecimalValueTextInputEditText.text.toString())
 
-        if (code == OutputCode.Success) {
-            binding.fragmentHexadecimalColorPickerCoordinatorLayout.showSnackbar("Successfully copied to clipboard", SnackbarDuration.Short)
-        } else {
-            binding.fragmentHexadecimalColorPickerCoordinatorLayout.showSnackbar("Failed to copy text clipboard", SnackbarDuration.Short)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            if (code == OutputCode.Success) {
+                binding.fragmentHexadecimalColorPickerCoordinatorLayout.showSnackbar(
+                    "Successfully copied to clipboard",
+                    SnackbarDuration.Short
+                )
+            } else {
+                binding.fragmentHexadecimalColorPickerCoordinatorLayout.showSnackbar(
+                    "Failed to copy text clipboard",
+                    SnackbarDuration.Short
+                )
+            }
         }
     }
 
--- app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/hex/HexadecimalColorPickerFragment.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/hex/HexadecimalColorPickerFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import com.therealbluepandabear.pixapencil.databinding.FragmentHexadecimalColorPickerBinding
