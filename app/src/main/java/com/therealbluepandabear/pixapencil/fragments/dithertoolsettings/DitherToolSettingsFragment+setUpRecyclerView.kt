45833926baf9d2bdf93913fd4865403fed3ab85d
therealbluepandabear <justaguyoney@gmail.com>
2022-06-04 19:22:18 +1200

    Bugfix.

--- app/src/main/java/com/therealbluepandabear/pixapencil/fragments/dithertoolsettings/DitherToolSettingsFragment+setUpRecyclerView.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/fragments/dithertoolsettings/DitherToolSettingsFragment+setUpRecyclerView.kt
@@ -1,20 +1,12 @@
 package com.therealbluepandabear.pixapencil.fragments.dithertoolsettings
 
-import android.content.res.Configuration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.therealbluepandabear.pixapencil.adapters.DitherBrushesAdapter
 import com.therealbluepandabear.pixapencil.database.DitherBrushDatabase
 
 fun DitherToolSettingsFragment.setUpRecyclerView() {
     val layoutManager = LinearLayoutManager(activity)
-
-    val layoutManagerOrientation: Int = if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-        LinearLayoutManager.HORIZONTAL
-    } else {
-        LinearLayoutManager.VERTICAL
-    }
-
-    layoutManager.orientation = layoutManagerOrientation
+    layoutManager.orientation = LinearLayoutManager.HORIZONTAL
 
     binding.fragmentDitherBrushesRecyclerView.layoutManager = layoutManager
     binding.fragmentDitherBrushesRecyclerView.adapter = DitherBrushesAdapter(DitherBrushDatabase.toList(), this, this@setUpRecyclerView.requireContext())
