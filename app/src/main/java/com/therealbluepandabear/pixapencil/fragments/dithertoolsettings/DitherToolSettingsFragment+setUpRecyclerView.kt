c63fdb94c138c45e6dd85784be7e43e25bd43231
therealbluepandabear <justaguyoney@gmail.com>
2022-06-11 16:46:50 +1200

    Minor dither tool settings UI change.

--- app/src/main/java/com/therealbluepandabear/pixapencil/fragments/dithertoolsettings/DitherToolSettingsFragment+setUpRecyclerView.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/fragments/dithertoolsettings/DitherToolSettingsFragment+setUpRecyclerView.kt
@@ -1,11 +1,12 @@
 package com.therealbluepandabear.pixapencil.fragments.dithertoolsettings
 
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.therealbluepandabear.pixapencil.adapters.DitherBrushesAdapter
 import com.therealbluepandabear.pixapencil.database.DitherBrushDatabase
 
 fun DitherToolSettingsFragment.setUpRecyclerView() {
-    val layoutManager = LinearLayoutManager(activity)
+    val layoutManager = GridLayoutManager(activity, 3)
     layoutManager.orientation = LinearLayoutManager.HORIZONTAL
 
     binding.fragmentDitherBrushesRecyclerView.layoutManager = layoutManager
