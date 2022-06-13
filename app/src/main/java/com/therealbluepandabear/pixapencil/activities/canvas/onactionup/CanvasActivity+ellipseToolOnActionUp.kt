23987f9e2e157468813987de4ad768f6de5b65e6
therealbluepandabear <justaguyoney@gmail.com>
2022-06-13 15:37:04 +1200

    Major fix #127. As in prior commits, MAY contain further bugs.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onactionup/CanvasActivity+ellipseToolOnActionUp.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onactionup/CanvasActivity+ellipseToolOnActionUp.kt
@@ -1,20 +1,25 @@
 package com.therealbluepandabear.pixapencil.activities.canvas.onactionup
 
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
+import com.therealbluepandabear.pixapencil.activities.canvas.canvascommands.overrideSetPixel
+import com.therealbluepandabear.pixapencil.activities.canvas.getSelectedColor
 import com.therealbluepandabear.pixapencil.activities.canvas.onpixeltapped.coordinates
 import com.therealbluepandabear.pixapencil.activities.canvas.onpixeltapped.ellipseOrigin
 import com.therealbluepandabear.pixapencil.activities.canvas.onpixeltapped.first
 import com.therealbluepandabear.pixapencil.algorithms.EllipseAlgorithm
 import com.therealbluepandabear.pixapencil.enums.Tool
+import com.therealbluepandabear.pixapencil.fragments.canvas.pixelGridViewInstance
 
 fun CanvasActivity.ellipseToolOnActionUp() {
     if (viewModel.currentTool == Tool.EllipseTool && viewModel.currentTool.outlined == false) {
         val circleAlgorithmInstance = EllipseAlgorithm(primaryAlgorithmInfoParameter, true)
 
-        if (ellipseOrigin!!.x > coordinates!!.x) {
-            circleAlgorithmInstance.compute(coordinates!!, ellipseOrigin!!)
-        } else {
-            circleAlgorithmInstance.compute(ellipseOrigin!!, coordinates!!)
+        if (ellipseOrigin != null && coordinates != null) {
+            if (ellipseOrigin!!.x > coordinates!!.x) {
+                circleAlgorithmInstance.compute(coordinates!!, ellipseOrigin!!)
+            } else {
+                circleAlgorithmInstance.compute(ellipseOrigin!!, coordinates!!)
+            }
         }
     }
 
