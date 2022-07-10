baac366bab6367d7c49e28bca571b5065f2d63bb
therealbluepandabear <justaguyoney@gmail.com>
2022-07-10 12:21:42 +1200

    Added `PickerToolTests`.

---
+++ app/src/androidTest/java/com/therealbluepandabear/pixapencil/tooltests/PickerToolTests.kt
@@ -0,0 +1,39 @@
+package com.therealbluepandabear.pixapencil.tooltests
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
+import com.therealbluepandabear.pixapencil.activities.canvas.binding
+import com.therealbluepandabear.pixapencil.activities.canvas.canvascommands.overrideSetPixel
+import com.therealbluepandabear.pixapencil.activities.canvas.getSelectedColor
+import com.therealbluepandabear.pixapencil.enums.Tool
+import com.therealbluepandabear.pixapencil.models.Coordinates
+import com.therealbluepandabear.pixapencil.tooltests.helper.ToolTestsHelper
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class PickerToolTests {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(CanvasActivity::class.java)
+
+    @Test
+    fun ptt_1() {
+        activityRule.scenario.onActivity {
+            ToolTestsHelper.prepare(it)
+
+            it.viewModel.currentTool = Tool.ColorPickerTool
+
+            it.canvasCommandsHelperInstance.overrideSetPixel(Coordinates(0,0), Color.BLUE)
+            it.onPixelTapped(Coordinates(0, 0))
+
+            assert((binding.activityCanvasColorPrimaryView.background as ColorDrawable).color == Color.BLUE)
+            assert(it.getSelectedColor() == Color.BLUE)
+        }
+    }
+}
