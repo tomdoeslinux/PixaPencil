6c3942acec4ed9cc1e7c47e749d9a6f8e45fd2d5
therealbluepandabear <justaguyoney@gmail.com>
2022-07-22 11:21:26 +1200

    Set `windowSoftInputMode` to `adjustPan` for `CanvasActivity`.

--- app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/ColorPickerFragment.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/fragments/colorpicker/ColorPickerFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.core.view.MenuHost
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
