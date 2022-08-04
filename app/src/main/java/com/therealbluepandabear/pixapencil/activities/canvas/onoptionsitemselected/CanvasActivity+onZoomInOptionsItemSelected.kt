34a3cd6135a0c6e953d8c363c765251a1d478509
therealbluepandabear <justaguyoney@gmail.com>
2022-08-04 17:05:24 +1200

    Set max zoom in limit to one million, removed some redundant code.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onoptionsitemselected/CanvasActivity+onZoomInOptionsItemSelected.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onoptionsitemselected/CanvasActivity+onZoomInOptionsItemSelected.kt
@@ -3,13 +3,18 @@ package com.therealbluepandabear.pixapencil.activities.canvas.onoptionsitemselec
 import com.therealbluepandabear.pixapencil.R
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.activities.canvas.oncreate.menu.ZOOM_INCREMENT
+import com.therealbluepandabear.pixapencil.extensions.disable
 import com.therealbluepandabear.pixapencil.extensions.enable
 
 fun CanvasActivity.onZoomInOptionsItemSelected() {
     binding.activityCanvasCardView.scaleX += ZOOM_INCREMENT
     binding.activityCanvasCardView.scaleY += ZOOM_INCREMENT
-    
-    menu.findItem(R.id.activityCanvasTopAppMenu_zoom_out_item).enable()
+
+    if (binding.activityCanvasCardView.scaleX + ZOOM_INCREMENT > 1_000_000) {
+        menu.findItem(R.id.activityCanvasTopAppMenu_zoom_in_item).disable()
+    } else {
+        menu.findItem(R.id.activityCanvasTopAppMenu_zoom_out_item).enable()
+    }
 
     binding.activityCanvasPixelGridView.invalidate()
 }
--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onoptionsitemselected/CanvasActivity+onZoomOutOptionsItemSelected.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onoptionsitemselected/CanvasActivity+onZoomOutOptionsItemSelected.kt
@@ -4,20 +4,19 @@ import com.therealbluepandabear.pixapencil.R
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.activities.canvas.oncreate.menu.ZOOM_INCREMENT
 import com.therealbluepandabear.pixapencil.extensions.disable
+import com.therealbluepandabear.pixapencil.extensions.enable
 import java.math.RoundingMode
 
 fun CanvasActivity.onZoomOutOptionsItemSelected() {
-    val canZoomOut = binding.activityCanvasCardView.scaleX - ZOOM_INCREMENT > ZOOM_INCREMENT
-
-    if (canZoomOut) {
-        binding.activityCanvasCardView.scaleX -= ZOOM_INCREMENT
-        binding.activityCanvasCardView.scaleY -= ZOOM_INCREMENT
-    }
+    binding.activityCanvasCardView.scaleX -= ZOOM_INCREMENT
+    binding.activityCanvasCardView.scaleY -= ZOOM_INCREMENT
 
     val roundedZoom = ZOOM_INCREMENT.toBigDecimal().setScale(1, RoundingMode.UP).toDouble()
 
     if (binding.activityCanvasCardView.scaleX - ZOOM_INCREMENT < roundedZoom) {
         menu.findItem(R.id.activityCanvasTopAppMenu_zoom_out_item).disable()
+    } else {
+        menu.findItem(R.id.activityCanvasTopAppMenu_zoom_in_item).enable()
     }
 
     binding.activityCanvasPixelGridView.invalidate()
