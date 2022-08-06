a2e1e4000e2084f2be7d850a256292e11e82baa7
therealbluepandabear <justaguyoney@gmail.com>
2022-08-06 12:21:22 +1200

    Fixed unversioned file.

---
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onactionup/CanvasActivity+lineTool+onActionUp.kt
@@ -0,0 +1,10 @@
+package com.therealbluepandabear.pixapencil.activities.canvas.onactionup
+
+import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
+
+fun CanvasActivity.lineToolOnActionUp() {
+    shapeOrigin = null
+    firstShapeDrawn = false
+    coordinates = null
+    viewModel.bitmapActionData.add(viewModel.currentBitmapAction!!)
+}
