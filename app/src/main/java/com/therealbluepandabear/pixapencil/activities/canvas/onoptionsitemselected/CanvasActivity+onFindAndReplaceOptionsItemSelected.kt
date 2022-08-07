430fff8b4cd7eafc5c0faac218ca2cf1b20eb1e7
therealbluepandabear <justaguyoney@gmail.com>
2022-08-07 14:26:55 +1200

    Code simplification for scaling.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onoptionsitemselected/CanvasActivity+onFindAndReplaceOptionsItemSelected.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onoptionsitemselected/CanvasActivity+onFindAndReplaceOptionsItemSelected.kt
@@ -7,26 +7,13 @@ import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.activities.canvas.canvashelpers.drawPixelGridViewBitmap
 import com.therealbluepandabear.pixapencil.activities.canvas.canvashelpers.drawTransparentBackgroundViewBitmap
 import com.therealbluepandabear.pixapencil.activities.canvas.selectedColorPaletteIndex
-import com.therealbluepandabear.pixapencil.enums.SnackbarDuration
 import com.therealbluepandabear.pixapencil.extensions.getColors
-import com.therealbluepandabear.pixapencil.extensions.showSnackbar
+import com.therealbluepandabear.pixapencil.extensions.rotate
 import com.therealbluepandabear.pixapencil.fragments.replacecolor.ReplaceColorFragment
+import kotlin.math.abs
+import kotlin.math.ceil
 
 fun CanvasActivity.onFindAndReplaceOptionsItemSelected() {
-    val paramScaledWidth =
-        if (binding.activityCanvasPixelGridView.drawToBitmap().width <= drawPixelGridViewBitmap().width) {
-            drawPixelGridViewBitmap().width * (drawPixelGridViewBitmap().width / binding.activityCanvasPixelGridView.drawToBitmap().width)
-        } else {
-            drawPixelGridViewBitmap().width * (binding.activityCanvasPixelGridView.drawToBitmap().width / drawPixelGridViewBitmap().width)
-        }
-
-    val paramScaledHeight =
-        if (binding.activityCanvasPixelGridView.drawToBitmap().height <= drawPixelGridViewBitmap().height) {
-            drawPixelGridViewBitmap().height * (drawPixelGridViewBitmap().height / binding.activityCanvasPixelGridView.drawToBitmap().height)
-        } else {
-            drawPixelGridViewBitmap().height * (binding.activityCanvasPixelGridView.drawToBitmap().height / drawPixelGridViewBitmap().height)
-        }
-
     supportFragmentManager.commit {
         replace(
             R.id.activityCanvas_primaryFragmentHost, ReplaceColorFragment.newInstance(
@@ -34,8 +21,8 @@ fun CanvasActivity.onFindAndReplaceOptionsItemSelected() {
                 paramPixelGridViewBitmapSource = drawPixelGridViewBitmap(),
                 paramTransparentBitmapSource = drawTransparentBackgroundViewBitmap(),
                 paramSelectedColorPaletteIndex = selectedColorPaletteIndex,
-                paramScaledWidth = paramScaledWidth,
-                paramScaledHeight = paramScaledHeight
+                paramScaledWidth = binding.activityCanvasPixelGridView.drawToBitmap().rotate(ceil(abs(binding.activityCanvasCardView.rotation)).toInt(), viewModel.flipMatrix).width,
+                paramScaledHeight =  binding.activityCanvasPixelGridView.drawToBitmap().rotate(ceil(abs(binding.activityCanvasCardView.rotation)).toInt(), viewModel.flipMatrix).height
             )
         )
         addToBackStack(null)
