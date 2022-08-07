47e15e24a671239222728cab4c0697d2f3139ae6
therealbluepandabear <justaguyoney@gmail.com>
2022-08-07 15:09:25 +1200

    Removed outdated tests.

--- app/src/androidTest/java/com/therealbluepandabear/pixapencil/tooltests/helper/ToolTestsHelper.kt
+++ app/src/androidTest/java/com/therealbluepandabear/pixapencil/tooltests/helper/ToolTestsHelper.kt
@@ -4,11 +4,12 @@ import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.activities.canvas.initPrimaryAlgorithmInfoParameter
 import com.therealbluepandabear.pixapencil.enums.Tool
 import com.therealbluepandabear.pixapencil.models.BitmapAction
+import java.util.*
 
 object ToolTestsHelper {
     fun prepare(activity: CanvasActivity) {
         activity.viewModel.currentTool = Tool.CircleTool
-        activity.viewModel.undoStack = mutableListOf()
+        activity.viewModel.undoStack = Stack()
         activity.viewModel.currentBitmapAction = BitmapAction(mutableListOf())
         activity.initPrimaryAlgorithmInfoParameter()
     }
