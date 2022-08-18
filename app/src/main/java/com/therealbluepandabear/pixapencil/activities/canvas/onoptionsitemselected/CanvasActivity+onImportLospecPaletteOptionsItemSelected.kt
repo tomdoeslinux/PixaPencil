e30383978160fcad18e03545860d96485dac3035
therealbluepandabear <justaguyoney@gmail.com>
2022-08-18 17:25:02 +1200

    Use view binding for 'Import Lospec Palette' dialog functionality.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onoptionsitemselected/CanvasActivity+onImportLospecPaletteOptionsItemSelected.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/onoptionsitemselected/CanvasActivity+onImportLospecPaletteOptionsItemSelected.kt
@@ -1,12 +1,11 @@
 package com.therealbluepandabear.pixapencil.activities.canvas.onoptionsitemselected
 
 import android.graphics.Color
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.view.LayoutInflater
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.textfield.TextInputEditText
 import com.google.gson.Gson
 import com.google.gson.JsonParser
 import com.therealbluepandabear.pixapencil.R
@@ -14,8 +13,8 @@ import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.activities.canvas.selectedColorPaletteIndex
 import com.therealbluepandabear.pixapencil.converters.JsonConverter
 import com.therealbluepandabear.pixapencil.database.AppData
+import com.therealbluepandabear.pixapencil.databinding.ImportLospecPaletteAlertBinding
 import com.therealbluepandabear.pixapencil.enums.SnackbarDuration
-import com.therealbluepandabear.pixapencil.extensions.activity
 import com.therealbluepandabear.pixapencil.extensions.showSnackbarWithAction
 import com.therealbluepandabear.pixapencil.models.ColorPalette
 import kotlinx.coroutines.CoroutineScope
@@ -23,17 +22,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 fun CanvasActivity.onImportLospecPaletteOptionsItemSelected() {
-    val details: ConstraintLayout =
-        activity()?.layoutInflater?.inflate(R.layout.import_lospec_palette_alert, activity()?.findViewById(android.R.id.content),false)
-                as ConstraintLayout
+    val importLospecPaletteAlertBinding = ImportLospecPaletteAlertBinding.inflate(LayoutInflater.from(this))
 
     val alertDialog = MaterialAlertDialogBuilder(this, R.style.ThemeOverlay_App_MaterialAlertDialog)
         .setTitle(R.string.activityCanvasTopAppMenu_import_lospec_palette)
-        .setView(details)
+        .setView(importLospecPaletteAlertBinding.root)
         .setPositiveButton(R.string.generic_ok) { _, _ ->
-            if (details.findViewById<TextInputEditText>(R.id.importLospecPaletteAlert_palette_url_identifier_textInputEditText).text!!.isNotEmpty()) {
+            if (importLospecPaletteAlertBinding.importLospecPaletteAlertPaletteUrlIdentifierTextInputEditText.text!!.isNotEmpty()) {
             val queue = Volley.newRequestQueue(this)
-            val url = "https://lospec.com/palette-list/${details.findViewById<TextInputEditText>(R.id.importLospecPaletteAlert_palette_url_identifier_textInputEditText).text.toString()}.json"
+            val url = "https://lospec.com/palette-list/${importLospecPaletteAlertBinding.importLospecPaletteAlertPaletteUrlIdentifierTextInputEditText.text.toString()}.json"
 
             val stringRequest = StringRequest(
                 Request.Method.GET, url,
