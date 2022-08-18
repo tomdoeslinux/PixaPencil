28ba75c41e9ec7cb3833e430b779242894848d76
therealbluepandabear <justaguyoney@gmail.com>
2022-08-18 17:04:38 +1200

    Improved UI for the 'Input Name' dialog.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/oncreate/MainActivity+initActivityResultLauncher.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/oncreate/MainActivity+initActivityResultLauncher.kt
@@ -1,12 +1,14 @@
 package com.therealbluepandabear.pixapencil.activities.main.oncreate
 
 import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.textfield.TextInputLayout
 import com.therealbluepandabear.pixapencil.R
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.activities.main.MainActivity
+import com.therealbluepandabear.pixapencil.databinding.NameProjectAlertBinding
 import com.therealbluepandabear.pixapencil.extensions.hideSoftInput
 import com.therealbluepandabear.pixapencil.extensions.putUriExtra
 import com.therealbluepandabear.pixapencil.utility.constants.StringConstants
@@ -14,21 +16,23 @@ import com.therealbluepandabear.pixapencil.utility.constants.StringConstants
 fun MainActivity.initActivityResultLauncher() {
     galleryActivityLauncher = registerForActivityResult(ActivityResultContracts.OpenDocument()) {
         if (it != null) {
-            val textInput: TextInputLayout =
-                layoutInflater.inflate(R.layout.name_project_alert, findViewById(android.R.id.content),false) as TextInputLayout
-            val text = textInput.editText?.text
+            val nameProjectAlertBinding = NameProjectAlertBinding.inflate(LayoutInflater.from(this))
+
+            nameProjectAlertBinding.root.post {
+                // We do this so that there is no default top margin, which I personally find it ugly
+                (nameProjectAlertBinding.root.layoutParams as ViewGroup.MarginLayoutParams).topMargin = 0
+            }
 
             val alertDialog = MaterialAlertDialogBuilder(this, R.style.ThemeOverlay_App_MaterialAlertDialog)
                 .setTitle(R.string.dialog_open_image_enter_name_title)
-                .setView(textInput)
-                .setMessage(R.string.dialog_open_image_enter_name_text)
+                .setView(nameProjectAlertBinding.root)
                 .setPositiveButton(R.string.generic_ok) { _, _ ->
-                    textInput.editText?.hideSoftInput()
+                    nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.hideSoftInput()
 
-                    if (text?.isNotEmpty() == true) {
+                    if (nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.text?.isNotEmpty() == true) {
                         startActivity(
                             Intent(this, CanvasActivity::class.java)
-                                .putExtra(StringConstants.Extras.PROJECT_TITLE_EXTRA, text.toString())
+                                .putExtra(StringConstants.Extras.PROJECT_TITLE_EXTRA, nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.text.toString())
                                 .putExtra(StringConstants.Extras.SPOTLIGHT_IN_PROGRESS_EXTRA, false)
                                 .putUriExtra(StringConstants.Extras.BITMAP_URI_EXTRA, it)
                         )
