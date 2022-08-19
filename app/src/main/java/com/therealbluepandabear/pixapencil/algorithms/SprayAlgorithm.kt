12683c212178b919664b72f1f7658f25df9ffde2
therealbluepandabear <justaguyoney@gmail.com>
2022-08-20 11:33:27 +1200

    Spray tool now generates points within a circle, instead of within a square. (Fix #215.)

--- app/src/main/java/com/therealbluepandabear/pixapencil/algorithms/SprayAlgorithm.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/algorithms/SprayAlgorithm.kt
@@ -2,24 +2,25 @@ package com.therealbluepandabear.pixapencil.algorithms
 
 import com.therealbluepandabear.pixapencil.activities.canvas.canvascommands.overrideSetPixel
 import com.therealbluepandabear.pixapencil.models.Coordinates
-import kotlin.math.floor
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 import kotlin.random.Random
 
 class SprayAlgorithm(private val algorithmInfo: AlgorithmInfoParameter, private var radius: Int, private var strength: Int) {
     fun compute(seed: Coordinates) {
-        val ct = floor((radius / 2).toDouble()).toInt()
-
-        val p1 = Coordinates(seed.x + ct, seed.y + ct)
-        val p2 = Coordinates(seed.x - ct, seed.y - ct)
-
         for (i in 0 until strength) {
-            val rn1 = Random.nextInt(-ct, ct)
-            val rn2 = Random.nextInt(-ct, ct)
-            val rn3 = Random.nextInt(-ct, ct)
-            val rn4 = Random.nextInt(-ct, ct)
+            /** Thank you to to aioobe on StackOverflow for this solution.
+             *
+             * - [Link to aioobe' profile](https://stackoverflow.com/users/276052/aioobe)
+             * - [Original StackOverFlow post](https://stackoverflow.com/questions/5837572/generate-a-random-point-within-a-circle-uniformly)
+             * **/
 
-            val randX = Random.nextInt(p2.x + rn1, p1.x + rn2)
-            val randY = Random.nextInt(p2.y + rn3, p1.y + rn4)
+            val r = radius * sqrt(Random.nextDouble(0.0, 2.0))
+            val theta = Random.nextDouble(0.0, 2.0) * 2 * kotlin.math.PI
+
+            val randX = (seed.x + r * cos(theta)).toInt()
+            val randY = (seed.y + r * sin(theta)).toInt()
 
             val randCoordinates = Coordinates(randX, randY)
 
