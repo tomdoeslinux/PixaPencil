dd819a21e14319c155c28976c6fed2a33309fcb6
therealbluepandabear <justaguyoney@gmail.com>
2022-08-22 17:11:20 +1200

    Migrated to view binding.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/bottomsheet/MainActivity+onViewDetailsTapped.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/bottomsheet/MainActivity+onViewDetailsTapped.kt
@@ -1,28 +1,25 @@
 package com.therealbluepandabear.pixapencil.activities.main.bottomsheet
 
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.view.LayoutInflater
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.therealbluepandabear.pixapencil.R
 import com.therealbluepandabear.pixapencil.activities.main.MainActivity
 import com.therealbluepandabear.pixapencil.converters.BitmapConverter
-import com.therealbluepandabear.pixapencil.extensions.activity
+import com.therealbluepandabear.pixapencil.databinding.ProjectDetailsAlertBoxBinding
 import com.therealbluepandabear.pixapencil.extensions.getNumberOfUniqueColors
 import com.therealbluepandabear.pixapencil.models.PixelArt
 
 fun MainActivity.extendedOnViewDetailsTapped(pixelArt: PixelArt) {
-    val details: ConstraintLayout =
-        activity()?.layoutInflater?.inflate(R.layout.project_details_alert_box, activity()?.findViewById(android.R.id.content),false)
-                as ConstraintLayout
+    val detailsAlertBinding = ProjectDetailsAlertBoxBinding.inflate(LayoutInflater.from(this))
 
-    details.findViewById<TextView>(R.id.projectDetailsAlertBox_width).text = pixelArt.width.toString()
-    details.findViewById<TextView>(R.id.projectDetailsAlertBox_height).text = pixelArt.height.toString()
-    details.findViewById<TextView>(R.id.projectDetailsAlertBox_colors).text = BitmapConverter.convertStringToBitmap(pixelArt.bitmap)?.getNumberOfUniqueColors().toString()
-    details.findViewById<TextView>(R.id.projectDetailsAlertBox_created).text = pixelArt.dateCreated
+    detailsAlertBinding.projectDetailsAlertBoxWidth.text = pixelArt.width.toString()
+    detailsAlertBinding.projectDetailsAlertBoxHeight.text = pixelArt.height.toString()
+    detailsAlertBinding.projectDetailsAlertBoxColors.text = BitmapConverter.convertStringToBitmap(pixelArt.bitmap)?.getNumberOfUniqueColors().toString()
+    detailsAlertBinding.projectDetailsAlertBoxCreated.text = pixelArt.dateCreated
 
     val alertDialog = MaterialAlertDialogBuilder(this, R.style.ThemeOverlay_App_MaterialAlertDialog)
         .setTitle(R.string.dialog_project_details_title)
-        .setView(details)
+        .setView(detailsAlertBinding.root)
         .setPositiveButton(R.string.generic_ok, null)
 
     alertDialog.show()
