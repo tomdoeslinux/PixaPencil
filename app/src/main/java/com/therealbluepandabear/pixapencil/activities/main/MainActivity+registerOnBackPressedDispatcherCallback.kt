9f0f375c92a213c97126f644da99db467eb690e7
therealbluepandabear <justaguyoney@gmail.com>
2022-08-23 16:10:00 +1200

    Fixed bug in which pressing the back button in `MainActivity` would not close the app.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+registerOnBackPressedDispatcherCallback.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/MainActivity+registerOnBackPressedDispatcherCallback.kt
@@ -8,11 +8,13 @@ fun MainActivity.registerOnBackPressedDispatcherCallback() {
         this,
         object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                // If we remove this, when the user would press the back button the bottom navigation view will disappear
-                binding.activityMainBottomNavigationView.visibility = View.VISIBLE
-
                 if (supportFragmentManager.backStackEntryCount > 0) {
+                    // If we remove this, when the user would press the back button the bottom navigation view will disappear
+                    binding.activityMainBottomNavigationView.visibility = View.VISIBLE
+
                     supportFragmentManager.popBackStackImmediate()
+                } else {
+                    this@registerOnBackPressedDispatcherCallback.finishAffinity()
                 }
             }
         })
