ae3075be698c89d94d42215574385507c33bed01
therealbluepandabear <justaguyoney@gmail.com>
2022-09-07 17:53:37 +1300

    Accept internal Skia bug for the time being (2) + remove redundant comments.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/canvashelpers/CanvasActivity+flip.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/canvashelpers/CanvasActivity+flip.kt
@@ -21,31 +21,24 @@ package com.therealbluepandabear.pixapencil.activities.canvas.canvashelpers
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.enums.FlipValue
 
-/**
- * There is a bug in which when you rotate a view 180 degrees, its drop shadow disappears.
- * This has been reported to Google but it hasn't been fixed in some compat libraries.
- * Hopefully it gets fixed in other libraries soon, but for now we can simply rotate it by 179.9 degrees and it seems to fix it.
- * The difference isn't noticeable.
- */
-
 fun CanvasActivity.flip(flipValue: FlipValue) {
     if (viewModel.flipMatrix.isEmpty() || viewModel.flipMatrix.last() != flipValue) {
         viewModel.flipMatrix.add(flipValue)
     }
 
     if (flipValue == FlipValue.Horizontal) {
-        if (binding.activityCanvasCardView.rotationY == 179.9f) {
+        if (binding.activityCanvasCardView.rotationY == 180f) {
             binding.activityCanvasCardView.rotationY = 0f
             viewModel.flipMatrix.removeLast()
         } else {
-            binding.activityCanvasCardView.rotationY = 179.9f
+            binding.activityCanvasCardView.rotationY = 180f
         }
     } else {
-        if (binding.activityCanvasCardView.rotationX == 179.9f) {
+        if (binding.activityCanvasCardView.rotationX == 180f) {
             binding.activityCanvasCardView.rotationX = 0f
             viewModel.flipMatrix.removeLast()
         } else {
-            binding.activityCanvasCardView.rotationX = 179.9f
+            binding.activityCanvasCardView.rotationX = 180f
         }
     }
 }
--- app/src/main/java/com/therealbluepandabear/pixapencil/enums/RotationValue.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/enums/RotationValue.kt
@@ -18,13 +18,6 @@
 
 package com.therealbluepandabear.pixapencil.enums
 
-/**
- * There is a bug in which when you rotate a view 180 degrees, its drop shadow disappears.
- * This has been reported to Google but it hasn't been fixed in some compat libraries.
- * Hopefully it gets fixed in other libraries soon, but for now we can simply rotate it by 179.9 degrees and it seems to fix it.
- * The difference isn't noticeable.
- */
-
 enum class RotationValue(val degrees: Float, val clockwise: Boolean = false) {
     NinetyAntiClockwise(90f),
     NinetyClockwise(90f, true),
