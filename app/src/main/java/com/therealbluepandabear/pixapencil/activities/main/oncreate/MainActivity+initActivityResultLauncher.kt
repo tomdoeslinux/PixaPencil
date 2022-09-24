da7524f935e76757ec3d80e5c433496ba65e41a7
therealbluepandabear <justaguyoney@gmail.com>
2022-09-24 14:13:15 +1300

    Fix #249; fix comment typo.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/oncreate/MainActivity+initActivityResultLauncher.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/main/oncreate/MainActivity+initActivityResultLauncher.kt
@@ -18,10 +18,12 @@
 
 package com.therealbluepandabear.pixapencil.activities.main.oncreate
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.doAfterTextChanged
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.therealbluepandabear.pixapencil.R
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
@@ -32,33 +34,46 @@ import com.therealbluepandabear.pixapencil.extensions.putUriExtra
 import com.therealbluepandabear.pixapencil.utility.constants.StringConstants
 
 fun MainActivity.initActivityResultLauncher() {
-    galleryActivityLauncher = registerForActivityResult(ActivityResultContracts.OpenDocument()) {
-        if (it != null) {
+    galleryActivityLauncher = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+        if (uri != null) {
             val nameProjectAlertBinding = NameProjectAlertBinding.inflate(LayoutInflater.from(this))
 
             nameProjectAlertBinding.root.post {
-                // We do this so that there is no default top margin, which I personally find it ugly
+                // We do this so that there is no default top margin, which I personally find ugly
                 (nameProjectAlertBinding.root.layoutParams as ViewGroup.MarginLayoutParams).topMargin = 0
             }
 
-            val alertDialog = MaterialAlertDialogBuilder(this, R.style.ThemeOverlay_App_MaterialAlertDialog)
+            nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.doAfterTextChanged {
+                if (nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.text.toString().isNotBlank()) {
+                    nameProjectAlertBinding.nameProjectAlertNameTextInputLayout.isErrorEnabled = false
+                }
+            }
+
+            val dialogBuilder = MaterialAlertDialogBuilder(this, R.style.ThemeOverlay_App_MaterialAlertDialog)
                 .setTitle(R.string.dialog_open_image_enter_name_title)
                 .setView(nameProjectAlertBinding.root)
-                .setPositiveButton(R.string.generic_ok) { _, _ ->
-                    nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.hideSoftInput()
-
-                    if (nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.text?.isNotEmpty() == true) {
-                        startActivity(
-                            Intent(this, CanvasActivity::class.java)
-                                .putExtra(StringConstants.Extras.PROJECT_TITLE_EXTRA, nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.text.toString())
-                                .putExtra(StringConstants.Extras.SPOTLIGHT_IN_PROGRESS_EXTRA, false)
-                                .putUriExtra(StringConstants.Extras.BITMAP_URI_EXTRA, it)
-                        )
-                    }
-                }
+                .setCancelable(false)
+                .setPositiveButton(R.string.generic_ok, null)
                 .setNegativeButton(R.string.generic_cancel, null)
 
-            alertDialog.show()
+            val dialog = dialogBuilder.show()
+
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                if (nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.text.toString().isNotBlank()) {
+                    nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.hideSoftInput()
+
+                    startActivity(
+                        Intent(this, CanvasActivity::class.java)
+                            .putExtra(StringConstants.Extras.PROJECT_TITLE_EXTRA, nameProjectAlertBinding.nameProjectAlertNameTextInputEditText.text.toString())
+                            .putExtra(StringConstants.Extras.SPOTLIGHT_IN_PROGRESS_EXTRA, false)
+                            .putUriExtra(StringConstants.Extras.BITMAP_URI_EXTRA, uri)
+                    )
+
+                    dialog.dismiss()
+                } else {
+                    nameProjectAlertBinding.nameProjectAlertNameTextInputLayout.error = getString(R.string.exception_invalid_project_name)
+                }
+            }
         }
     }
 }
