f389a2d53c58bed63d214233db545c3a075aeb73
therealbluepandabear <therealbluepandabear@protonmail.com>
2023-04-11 13:05:42 +1200

    Upgrade Gradle; cleanup for `extendedOnToolLongTapped` function.

--- app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/ontapped/CanvasActivity+onToolLongTapped.kt
+++ app/src/main/java/com/therealbluepandabear/pixapencil/activities/canvas/ontapped/CanvasActivity+onToolLongTapped.kt
@@ -18,156 +18,133 @@
 
 package com.therealbluepandabear.pixapencil.activities.canvas.ontapped
 
+import androidx.annotation.StringRes
 import com.therealbluepandabear.pixapencil.R
 import com.therealbluepandabear.pixapencil.activities.canvas.CanvasActivity
 import com.therealbluepandabear.pixapencil.enums.Tool
 import com.therealbluepandabear.pixapencil.extensions.showSimpleInfoDialog
 import com.therealbluepandabear.pixapencil.utility.constants.StringConstants
 
+data class ToolInfo(
+    val identifier: String,
+    @StringRes val titleResId: Int,
+    @StringRes val descriptionResId: Int,
+    val tool: Tool
+)
+
+val toolInfos = listOf(
+    ToolInfo(
+        identifier = StringConstants.Identifiers.PENCIL_TOOL_IDENTIFIER,
+        titleResId = R.string.pencil_tool_info_title,
+        descriptionResId = R.string.pencil_tool_info_text,
+        tool = Tool.PencilTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.FILL_TOOL_IDENTIFIER,
+        titleResId = R.string.fill_tool_info_title,
+        descriptionResId = R.string.fill_tool_info_text,
+        tool = Tool.FillTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.MOVE_TOOL_IDENTIFIER,
+        titleResId = R.string.move_tool_info_title,
+        descriptionResId = R.string.move_tool_info_text,
+        tool = Tool.MoveTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.LINE_TOOL_IDENTIFIER,
+        titleResId = R.string.line_tool_info_title,
+        descriptionResId = R.string.line_tool_info_text,
+        tool = Tool.LineTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.RECTANGLE_TOOL_IDENTIFIER,
+        titleResId = R.string.rectangle_tool_info_title,
+        descriptionResId = R.string.rectangle_tool_info_text,
+        tool = Tool.RectangleTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.OUTLINED_RECTANGLE_TOOL_IDENTIFIER,
+        titleResId = R.string.outlined_rectangle_tool_info_title,
+        descriptionResId = R.string.outlined_rectangle_tool_info_text,
+        tool = Tool.OutlinedRectangleTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.SQUARE_TOOL_IDENTIFIER,
+        titleResId = R.string.square_tool_info_title,
+        descriptionResId = R.string.square_tool_info_text,
+        tool = Tool.SquareTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.OUTLINED_SQUARE_TOOL_IDENTIFIER,
+        titleResId = R.string.outlined_square_tool_info_title,
+        descriptionResId = R.string.outlined_square_tool_info_text,
+        tool = Tool.OutlinedSquareTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.ELLIPSE_TOOL_IDENTIFIER,
+        titleResId = R.string.ellipse_tool_info_title,
+        descriptionResId = R.string.ellipse_tool_info_text,
+        tool = Tool.EllipseTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.OUTLINED_ELLIPSE_TOOL_IDENTIFIER,
+        titleResId = R.string.outlined_ellipse_tool_info_title,
+        descriptionResId = R.string.outlined_ellipse_tool_info_text,
+        tool = Tool.OutlinedEllipseTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.CIRCLE_TOOL_IDENTIFIER,
+        titleResId = R.string.circle_tool_info_title,
+        descriptionResId = R.string.circle_tool_info_text,
+        tool = Tool.CircleTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.OUTLINED_CIRCLE_TOOL_IDENTIFIER,
+        titleResId = R.string.outlined_circle_tool_info_title,
+        descriptionResId = R.string.outlined_circle_tool_info_text,
+        tool = Tool.OutlinedCircleTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.SPRAY_TOOL_IDENTIFIER,
+        titleResId = R.string.spray_tool_info_title,
+        descriptionResId = R.string.spray_tool_info_text,
+        tool = Tool.SprayTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.POLYGON_TOOL_IDENTIFIER,
+        titleResId = R.string.polygon_tool_info_title,
+        descriptionResId = R.string.polygon_tool_info_text,
+        tool = Tool.PolygonTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.DITHER_TOOL_IDENTIFIER,
+        titleResId = R.string.dither_tool_info_title,
+        descriptionResId = R.string.dither_tool_info_text,
+        tool = Tool.DitherTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.SHADING_TOOL_IDENTIFIER,
+        titleResId = R.string.shading_tool_info_title,
+        descriptionResId = R.string.shading_tool_info_text,
+        tool = Tool.ShadingTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.COLOR_PICKER_TOOL_IDENTIFIER,
+        titleResId = R.string.color_picker_tool_info_title,
+        descriptionResId = R.string.color_picker_tool_info_text,
+        tool = Tool.ColorPickerTool
+    ),
+    ToolInfo(
+        identifier = StringConstants.Identifiers.ERASE_TOOL_IDENTIFIER,
+        titleResId = R.string.erase_tool_info_title,
+        descriptionResId = R.string.erase_tool_info_text,
+        tool = Tool.EraseTool
+    )
+)
+
 fun CanvasActivity.extendedOnToolLongTapped(toolName: String) {
-    when (toolName) {
-        StringConstants.Identifiers.PENCIL_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.PencilTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.pencil_tool_info_title),
-                    getString(R.string.pencil_tool_info_text))
-            }
-        }
+    val toolInfo = toolInfos.find { it.identifier == toolName && it.tool == viewModel.currentTool } ?: return
 
-        StringConstants.Identifiers.FILL_TOOL_IDENTIFIER  -> {
-            if (viewModel.currentTool == Tool.FillTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.fill_tool_info_title),
-                    getString(R.string.fill_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.MOVE_TOOL_IDENTIFIER  -> {
-            if (viewModel.currentTool == Tool.MoveTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.move_tool_info_title),
-                    getString(R.string.move_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.LINE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.LineTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.line_tool_info_title),
-                    getString(R.string.line_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.RECTANGLE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.RectangleTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.rectangle_tool_info_title),
-                    getString(R.string.rectangle_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.OUTLINED_RECTANGLE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.OutlinedRectangleTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.outlined_rectangle_tool_info_title),
-                    getString(R.string.outlined_rectangle_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.SQUARE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.SquareTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.square_tool_info_title),
-                    getString(R.string.square_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.OUTLINED_SQUARE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.OutlinedSquareTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.outlined_square_tool_info_title),
-                    getString(R.string.outlined_square_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.ELLIPSE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.EllipseTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.ellipse_tool_info_title),
-                    getString(R.string.ellipse_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.OUTLINED_ELLIPSE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.OutlinedEllipseTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.outlined_ellipse_tool_info_title),
-                    getString(R.string.outlined_ellipse_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.CIRCLE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.CircleTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.circle_tool_info_title),
-                    getString(R.string.circle_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.OUTLINED_CIRCLE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.OutlinedCircleTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.outlined_circle_tool_info_title),
-                    getString(R.string.outlined_circle_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.SPRAY_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.SprayTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.spray_tool_info_title),
-                    getString(R.string.spray_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.POLYGON_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.PolygonTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.polygon_tool_info_title),
-                    getString(R.string.polygon_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.DITHER_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.DitherTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.dither_tool_info_title),
-                    getString(R.string.dither_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.SHADING_TOOL_IDENTIFIER  -> {
-            if (viewModel.currentTool == Tool.ShadingTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.shading_tool_info_title),
-                    getString(R.string.shading_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.COLOR_PICKER_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.ColorPickerTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.color_picker_tool_info_title),
-                    getString(R.string.color_picker_tool_info_text))
-            }
-        }
-
-        StringConstants.Identifiers.ERASE_TOOL_IDENTIFIER -> {
-            if (viewModel.currentTool == Tool.EraseTool) {
-                showSimpleInfoDialog(
-                    getString(R.string.erase_tool_info_title),
-                    getString(R.string.erase_tool_info_text))
-            }
-        }
-    }
+    showSimpleInfoDialog(getString(toolInfo.titleResId), getString(toolInfo.descriptionResId))
 }
