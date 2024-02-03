5eb4575438072c091c29ed663f545654ec2b26c6
tom <tomdoeslinux@protonmail.com>
2024-02-03 22:55:52 +1300

    Add `react-native-paper` dependency, add app header.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Appbar, Button, PaperProvider} from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Appbar.Header style={styles.appHeader}>
+        <Appbar.Content title="PixaPencil" />
+        <Appbar.Action icon="calendar" onPress={() => {}} />
+        <Appbar.Action icon="magnify" onPress={() => {}} />
+      </Appbar.Header>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appHeader: {
+    backgroundColor: '#f0f4fc',
+    elevation: 4,
   },
 });
