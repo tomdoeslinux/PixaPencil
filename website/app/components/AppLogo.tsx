6d027e9ba2f041c887c13a724002402dddb7b121
tom <tomdoeslinux@protonmail.com>
2024-02-03 22:17:49 +1300

    Cleanup.

--- website/app/components/AppLogo.tsx
+++
@@ -1,6 +0,0 @@
-import { Image } from '@chakra-ui/react'
-import React from 'react'
-
-export default function AppLogo() {
-  return <Image width='40px' height='40px' borderRadius='10px' src='/pixapencil-logo.jpg' />
-}
