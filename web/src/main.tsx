02db6d4aa859c376253bfaa61db7981b95c037e6
tomdoeslinux <tomdoeslinux@protonmail.com>
2024-05-15 09:41:40 +1200

    Add ChakraUI dependency.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
